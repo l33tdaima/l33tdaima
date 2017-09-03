f3f05b774c2fee3388b99364907e9a4e268e8d90
l33tdaima <jwmiany@gmail.com>
2017-09-02 23:23:52 -0400

    Solved p049m and started p056m

---
+++ p056m/merge_interval.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for an interval.
+ * function Interval(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ * }
+ */
+/**
+ * @param {Interval[]} intervals
+ * @return {Interval[]}
+ */
+
+ function Interval(start, end) {
+    this.start = start;
+    this.end = end;
+}
+
+var merge = function(intervals) {
+    
+};
+
+var testInteval = new Interval(0, 1);
+console.log(testInteval.start, testInteval.end);

cefae45441617052eb4a54e980964978b5133ed8
l33tdaima <jwmiany@gmail.com>
2017-11-12 11:52:23 -0500

    Solved p437e with two solutions

---
+++ p437e/path_sum3a.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+const tm = require('../p297h/serialize_binary_tree');
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number}
+ */
+var pathSum = function(root, sum) {
+    let numPath = 0;
+    var recPathSumHelper = function(p, s) {
+        if (p == null) {
+            return;
+        }
+        if (p.val === s) {
+            numPath ++;
+        }
+        recPathSumHelper(p.left, s - p.val);
+        recPathSumHelper(p.right, s - p.val);
+    };
+    var recPathSum = function(rt) {
+        if (rt == null) {
+            return;
+        }
+        recPathSumHelper(rt, sum);
+        recPathSum(rt.left);
+        recPathSum(rt.right);
+    };
+    recPathSum(root);
+    return numPath;
+};
+// TESTS
+[
+    ["#", 0],
+    ["1", 1],
+    ["1,-2,1,-1,#,#,3,#,#,-3,-2,#,#,#", -1],
+    ["10,5,3,3,#,#,-2,#,#,2,#,1,#,#,-3,#,11,#,#", 8]
+].forEach(function (test) {
+    let tree = tm.deserialize(test[0]);
+    console.log(test[0], "total num of path sum of", test[1],
+                "->", pathSum(tree, test[1]));
+    console.log("--------");
+});

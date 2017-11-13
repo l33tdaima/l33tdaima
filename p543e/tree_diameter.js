cc84f7c8c28d4f2f1b70230e969ab6683b013919
l33tdaima <jwmiany@gmail.com>
2017-11-12 22:07:31 -0500

    Solved p543e diameter

---
+++ p543e/tree_diameter.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+const tm = require('../p297h/serialize_binary_tree');
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+    var recDia = function(p) {
+        if (p == null) {
+            return { depth: 0,
+                     dia:   0 };
+        }
+        if (p.left === null && p.right === null) {
+            return { depth: 1,
+                     dia:   0 };
+        }
+        let lt = recDia(p.left);
+        let rt = recDia(p.right);
+        return { depth: Math.max(lt.depth, rt.depth) + 1,
+                 dia: Math.max(lt.dia, rt.dia, lt.depth + rt.depth)};
+    };
+    return recDia(root).dia;
+};
+// TEST
+[
+    "#",
+    "1,#,#",
+    "1,2,#,#,#",
+    "1,2,#,#,3,#,#",
+    "1,2,4,#,#,5,#,#,3,#,#",
+    "1,2,4,6,7,#,#,#,#,5,#,8,#,9,#,#,3,#,#"
+].forEach(function(test){
+    let tree = tm.deserialize(test);
+    console.log("Diagmeter of", test,
+                "->", diameterOfBinaryTree(tree));
+});

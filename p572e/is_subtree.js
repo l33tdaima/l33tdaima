2fdc02c3acfb1f8e6e0427f6d778d11f65146269
l33tdaima <jwmiany@gmail.com>
2017-11-13 09:13:46 -0500

    Solved p572e subtree

---
+++ p572e/is_subtree.js
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+const tm = require('../p297h/serialize_binary_tree');
+/** Is two tree equal
+ * @param {TreeNode} a
+ * @param {TreeNode} b
+ * @return {boolean}
+ */
+var isEqual = function(a, b) {
+    if (a === null && b === null) {
+        return true;
+    }
+    if (a === null ||  b === null) {
+        return false;
+    }
+    return (a.val === b.val) && 
+           isEqual(a.left, b.left) &&
+           isEqual(a.right, b.right);
+};
+/**
+ * @param {TreeNode} s
+ * @param {TreeNode} t
+ * @return {boolean}
+ */
+var isSubtree = function(s, t) {
+    if (t === null) {
+        return s === null;
+    }
+    if (s === null) { // t !== null here
+        return false;
+    }
+    if (s.val === t.val && isEqual(s, t)) {
+        return true;
+    } else {
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+};
+// TEST
+[
+    ["#", "#"],
+    ["1", "#"],
+    ["#", "1"],
+    ["3,4,1,#,#,2,#,#,5,#,#", "4,1,#,#,2,#,#"],
+    ["3,4,1,#,#,2,0,#,#,#,5,#,#", "4,1,#,#,2,#,#"],
+].forEach(function(test) {
+    let s = tm.deserialize(test[0]);
+    let t = tm.deserialize(test[1]);
+    console.log("Is", test[1], "a subtree of", test[0],
+                "->", isSubtree(s,t));
+});

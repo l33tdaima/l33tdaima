cef93238d923a455b3a0ddb5b1e9ddce2a9e89ab
l33tdaima <jwmiany@gmail.com>
2017-11-13 21:57:39 -0500

    Solved p617e merge trees

---
+++ p617e/merge_trees.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+const tm = require('../p297h/serialize_binary_tree');
+/**
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {TreeNode}
+ */
+var mergeTrees = function(t1, t2) {
+    if (t2 == null) {
+        // this is t referrencing t1, ideally deep copy
+        return t1;
+    }
+    let t = new tm.TreeNode(t2.val);
+    if (t1 !== null) {
+        t.val += t1.val;
+    }
+    t.left = mergeTrees(t1 ? t1.left : null, t2.left);
+    t.right = mergeTrees(t1 ? t1.right : null, t2.right);
+    return t;
+};
+// TEST
+[
+    ["#", "#"],
+    ["1", "#"],
+    ["#", "1"],
+    ["1,2,#,#,#", "1,#,3,#,#"],
+    ["1,#,#", "1,2,#,4,#,#,3,#,#"],
+    ["1,3,5,#,#,#,2,#,#", "2,1,#,4,#,#,3,#,#"],
+].forEach(function(test) {
+    let t1 = tm.deserialize(test[0]);
+    let t2 = tm.deserialize(test[1]);
+    let t = mergeTrees(t1, t2);
+    console.log("Merged tree ->", tm.serialize(t));
+});

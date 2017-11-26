2892742e3481fa8992e6933969667ce26370939f
l33tdaima <jwmiany@gmail.com>
2017-11-26 16:56:18 -0500

    Solved p535m tinyURL

---
+++ p535m/tinyurl.js
@@ -0,0 +1,48 @@
+const charmap = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+var short2long = {};
+
+/**
+ * Encodes a URL to a shortened URL.
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+var encode = function(longUrl) {
+    var genShortUrl = function () {
+        let rand = [];
+        for (let i = 0; i < 6; ++i) {
+            rand.push( charmap[~~(Math.random() * 61)] );
+        }
+        return rand.join('');
+    }
+    let shortUrl = null;
+    do {
+        shortUrl = genShortUrl();
+    } while (short2long[shortUrl] !== undefined);
+    short2long[shortUrl] = longUrl;
+    return shortUrl;
+};
+
+/**
+ * Decodes a shortened URL to its original URL.
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+var decode = function(shortUrl) {
+    return short2long[shortUrl];
+};
+
+/**
+ * Your functions will be called as such:
+ * decode(encode(url));
+ */
+[
+    "",
+    "a",
+    "https://leetcode.com/problems/design-tinyurl",
+].forEach(function (test) {
+    console.log("TinyURL of", test, "->", encode(test));
+    console.assert(decode(encode(test)) === test);
+});

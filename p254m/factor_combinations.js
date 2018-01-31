b87f02b21ca21c3fe13a610ae2e3b5b1b4a3451d
l33tdaima <jwmiany@gmail.com>
2018-02-01 00:14:38 +0100

    Solved p254m get factor combinations

---
+++ p254m/factor_combinations.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} n
+ * @param {number} start: starting number of iteration
+ * @return {number[][]}
+ */
+var recGetFactors = function(n, start) {
+    let results = new Array();
+    // set upper bound to sqrt(n) speed up 5x
+    for (let i = start; i*i <= n; ++i) { 
+        if (n % i !== 0) { continue; }
+        let f = n / i;
+        // push the unfactorized factor
+        if (f >= i) {
+            results.push([i, f]);
+        }
+        // recursion
+        let subfactors = recGetFactors(f, i);
+        /*
+        Array.prototype.push.apply(
+            results, 
+            subfactors.map((v) => [i].concat(v))
+        );*/
+        for (let j = 0, len = subfactors.length; j < len; ++j) {
+            subfactors[j].unshift(i);
+            results.push(subfactors[j]);
+        }
+    }
+    return results;
+};
+/**
+ * @param {number} n
+ * @return {number[][]}
+ */
+var getFactors = function(n) {
+    return recGetFactors(n, 2);
+};
+// TEST
+[
+    1, 37, 4, 6, 12, 32,
+].forEach(function (test) {
+    console.log("Get factors of", test, "->");
+    console.log("", getFactors(test));
+});

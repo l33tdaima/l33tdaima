fd2add6a8c3b797d37c21a724feaaf739b4adcb2
l33tdaima <jwmiany@gmail.com>
2018-02-12 23:30:40 -0500

    Solved p204e count primes

---
+++ p204e/count_primes.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    let isPrime = Array.from({length: n}, (v) => true);
+    let count = 0;
+    // sieve of Eratosthenes, if only need to filter out all non-primes
+    // we can simply stop at i <= sqrt(n)
+    for (let i = 2; i < n; ++i) {
+        if (isPrime[i]) {
+            for (let j = 2; i*j < n; ++j) {
+                isPrime[i*j] = false;
+            }
+            count ++;
+        }
+    }
+    return count;
+};
+// TEST
+[
+    [0,0],
+    [3,1],
+    [4,2],
+    [5,2],
+    [6,3],
+    [7,3],
+    [8,4],
+    [10,4],
+    [15,6],
+    [300,62],
+    [1000,168],
+    [5000,669],
+].forEach((test) => {
+    let c = countPrimes(test[0]);
+    console.log("# of primes less than", test[0], "->",
+                c, "|", c === test[1]);
+});

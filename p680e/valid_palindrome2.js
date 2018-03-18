d9c32ebc469ec6e5be9cf5e9a528e367120cb4b2
l33tdaima <jwmiany@gmail.com>
2018-03-17 23:15:49 -0400

    Solved p680e with hint

---
+++ p680e/valid_palindrome2.js
@@ -0,0 +1,48 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+    let isPalindrome = function(l, r) {
+        while (l < r) {
+            if (s[l++] !== s[r--]) { return false; }
+        }
+        return true;
+    };
+
+    let l = 0, r = s.length - 1;
+    while (l < r && s[l] === s[r]) {
+        l ++; r --;
+    }
+    if (l >= r) { return true; }
+    return isPalindrome(l+1, r) || isPalindrome(l, r-1);
+};
+
+var validPalindromeAtMostK = function(s, k) {
+    let isPalindrome = function(l, r, d) {
+        if (l >= r) { return true; }
+        if (s[l] === s[r]) {
+            return isPalindrome(l+1, r-1, d);
+        } else {
+            return d > 0 && (isPalindrome(l+1, r, d-1) || isPalindrome(l, r, d-1));
+        }
+    }
+    return isPalindrome(0, s.length - 1, k);
+};
+// TEST
+[
+    "aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga",
+    "",
+    "aaaa",
+    "aba",
+    "abca",
+    "abcea",
+    "bacca",
+    "accab",
+    "acdef",
+].forEach(t => {
+    console.log("Can we make", t, "a palindrome with at most 1 tremoval? ->",
+                validPalindrome(t), validPalindromeAtMostK(t, 1));
+    console.log("Can we make", t, "a palindrome with at most 2 tremoval? ->",
+                validPalindromeAtMostK(t, 2));
+});

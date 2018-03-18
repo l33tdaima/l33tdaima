3e2712aae75aeaa2acfe960752dfddc6b43f7e4a
l33tdaima <jwmiany@gmail.com>
2018-03-18 10:27:37 -0400

    C++ solution p001e

--- p001e/README.md
+++ p001e/README.md
@@ -1,4 +1,5 @@
 # 1. Two Sum (Easy)
+
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -9,5 +10,10 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 
-#Array #Hash Table
+## Solution
+Scan the array: 
+Adding pair<number, index> into a hash table and check if (target - number) already exists.
+
 #MSFT #FB #AMZN #BB #UBER #LNKD #APPL #AIRBNB #YELP #YHOO #ADBE #DROPBOX
+
+#Array #Hash Table
---
+++ p001e/two_sum.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <unordered_map>
+
+#include <initializer_list>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> ht;
+        for (int i = 0; i < nums.size(); ++i) {
+            int key = target - nums[i];
+            if (ht.find(key) != ht.end()) {
+                vector<int> res;
+                res.push_back(ht[key]);
+                res.push_back(i);
+                return res;
+            } else {
+                ht[nums[i]] = i;
+            }
+        }
+        return vector<int>();
+    }
+};
+
+// TEST
+struct Test {
+    initializer_list<int> numsIList;
+    int target;
+    
+    void run() {
+        vector<int> nums(numsIList);
+        Solution sol;
+        auto res = sol.twoSum(nums, target);
+        cout << "Two sum of [";
+        for (auto n : nums) { cout << n << ","; }
+        cout << "] target " << target << " -> (";
+        for (auto i : res) { cout << i << ","; }
+        cout << ")" << endl;
+    }
+};
+// g++ -std=c++11 two_sum.cpp -o test && ./test && rm test
+int main(int argc, char *argv[]) {
+    initializer_list<Test> tests = {
+        {{3,2,4}, 6},
+        {{2,1,3}, 3},
+        {{3,2,1,5,6,4}, 10},
+        {{3,2,1,5,6,4}, 6},
+    };
+    for (auto t : tests) { t.run(); }
+    return 0;
+}

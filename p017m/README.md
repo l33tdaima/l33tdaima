da3799cc78f913633cead3de1466c6065e6b71ae
l33tdaima <jwmiany@gmail.com>
2018-04-21 17:49:56 -0400

    Revisited p017m letter combinations with speed improvement using BFS

--- p017m/README.md
+++ p017m/README.md
@@ -7,4 +7,18 @@ A mapping of digit to letters (just like on the telephone buttons) is given belo
 Input:Digit string "23"
 Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 
-#Backtracking
+## Solution
+### DFS Approach (Slow)
+- Create a cartesian product helper to return the string list for two sets, each set is the mapping of a digit;
+- For any digit string, recursively do cartesian product of first and the rest of string.
+
+### BFS Approach (Fast)
+- Create output linked list used as a FIFO queue, add an empty string initially;
+- For each digit char in digits string
+  - Pop a base string from queue as long as its length is i, i = 0..n, (why? otherwise the string in the queue is pushed for next level)
+  - Push the concatenation of this string with mapped letter, e.g. 'a'+'d', 'a'+'e', 'a'+'f'
+- Return output linked list 
+
+#GOOGL #FB #AMZN #UBER #DBX
+
+#String #Backtracking
--- p017m/letter_combinations.js
+++ p017m/letter_combinations.js
@@ -3,8 +3,8 @@
  * @return {string[]}
  */
 var mapping = {
-    '0': [" "],
-    '1': [""],
+    '0': [],
+    '1': [],
     '2': ["a", "b", "c"],
     '3': ["d", "e", "f"],
     '4': ["g", "h", "i"],
@@ -14,16 +14,16 @@ var mapping = {
     '8': ["t", "u", "v"],
     '9': ["w", "x", "y", "z"]
 };
-var cartesianProduct = function(a, b) {
-    var product = [];
-    for (let i = 0; i < a.length; ++i) {
-        for (let j = 0; j < b.length; ++j) {
-            product.push(a[i].concat(b[j]));
+var letterCombinationsDFS = function(digits) {
+    let cartesianProduct = function(a, b) {
+        var product = [];
+        for (let i = 0; i < a.length; ++i) {
+            for (let j = 0; j < b.length; ++j) {
+                product.push(a[i].concat(b[j]));
+            }
         }
-    }
-    return product;
-}
-var letterCombinations = function(digits) {
+        return product;
+    };
     // console.log("Input digits: ", digits);
     if (digits.length === 1) {
         return mapping[digits[0]];
@@ -31,11 +31,26 @@ var letterCombinations = function(digits) {
     if (digits.length === 0) {
         return [];
     }
-    return cartesianProduct(mapping[digits[0]], letterCombinations(digits.substr(1)));
+    return cartesianProduct(mapping[digits[0]], letterCombinationsDFS(digits.substr(1)));
 };
-
-var tests = [ "", "1", "3", "23", "357", "2468"];
-
-tests.forEach(function(val) {
-    console.log(val + ": " + letterCombinations(val));
+var letterCombinationsBFS = function(digits) {
+    if (digits.length === 0 || digits === "1") { return []; }
+    let ans = [""];
+    for (let i = 0; i < digits.length; ++i) {
+        let d = digits[i];
+        while (ans[0].length === i) {
+            let base = ans.shift();
+            for (let c of mapping[d]) {
+                ans.push(base + c);
+            }
+        }
+    }
+    return ans;
+};
+[
+    "", "1", "3", "23", "357", "2468"
+].forEach(val => {
+    console.log("\"" + val + "\" -> ");
+    console.log("" + letterCombinationsBFS(val));
+    console.log("" + letterCombinationsDFS(val));
 }, this);

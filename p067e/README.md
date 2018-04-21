17350d664be29305f41358fb5a7e6ed796b9df8d
l33tdaima <jwmiany@gmail.com>
2018-04-21 15:08:05 -0400

    Revisited p067e add binary with improvement

--- p067e/README.md
+++ p067e/README.md
@@ -2,7 +2,24 @@
 
 Given two binary strings, return their sum (also a binary string).
 
-For example,
-a = "11"
-b = "1"
-Return "100".
+### Example 1:
+```
+Input: a = "11", b = "1"
+Output: "100"
+```
+
+### Example 2:
+```
+Input: a = "1010", b = "1011"
+Output: "10101"
+```
+
+## Solution
+- Scan from back of two string until reaching the begins of both and no more carry,
+  - compute the carry of each bit by adding previous carry to the current bit of a and b, fill 0 if already reaching to the beginning
+  - convert the carry number to '1' or '0'
+  - prefix the new carry character
+
+#FB
+
+#Math #String
--- p067e/add_binary.js
+++ p067e/add_binary.js
@@ -4,34 +4,24 @@
  * @return {string}
  */
 var addBinary = function(a, b) {
-    let resultArray = Array.from(
-        {length: Math.max(a.length, b.length)}, (v) => 0);
-    let carry = 0;
-    for (let ia = a.length - 1, ib = b.length - 1, ir = resultArray.length - 1;
-         ia >= 0 || ib >= 0; --ia, --ib, --ir) {
-        let bita = a.charAt(ia) === '1'? 1 : 0; // zero also if outofbound
-        let bitb = b.charAt(ib) === '1'? 1 : 0;
-        let add = bita + bitb + carry;
-        resultArray[ir] = add % 2;
-        carry = ~~(add / 2);
+    let ans = "";
+    let carry = 0, i = a.length - 1, j = b.length - 1;
+    while (i >= 0 || j >= 0 || carry === 1) {
+        carry += (i >= 0)? (a[i--] - '0') : 0;
+        carry += (j >= 0)? (b[j--] - '0') : 0;
+        ans = String.fromCharCode(carry % 2 + '0'.charCodeAt(0)) + ans;
+        carry = ~~(carry / 2);
     }
-    if (carry > 0) {
-        return "1" + resultArray.join('');
-    }
-    else {
-        return resultArray.join('');
-    }
+    return ans;
 };
-
-testArray = [
+// TEST
+[
     ["11", "1"],
     ["10", "1010"],
     ["110", "10001"],
     ["111111", "1"]
-];
-testArray.forEach(function(val) {
+].forEach(val => {
     let sum = addBinary(...val);
-    console.log(val[0], "+", val[1], "=", addBinary(...val),
-                ", Test passed:",
+    console.log(val[0], "+", val[1], "=", sum, ", Test passed?",
                 parseInt(val[0], 2) + parseInt(val[1], 2) === parseInt(sum, 2));
 });

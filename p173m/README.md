c133fa95d81ae1569d73830b55a574d8bff61e44
l33tdaima <jwmiany@gmail.com>
2018-04-22 12:43:17 -0400

    Revisited p173 BST Iterator with solution meet space restriction

--- p173m/README.md
+++ p173m/README.md
@@ -4,7 +4,31 @@ Implement an iterator over a binary search tree (BST). Your iterator will be ini
 
 Calling next() will return the next smallest number in the BST.
 
-Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+### Note:
+next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
+
+## Solution
+### Approach without memory limitation
+We can simply push values during initialization from larger to small to stack like
+```JS
+    var init = function (root, stack) {
+        if (root === null) { return; }
+        init(root.right, stack);
+        stack.push(root.val);
+        init(root.left, stack);
+    };
+    this.smallerValStack = new Array();
+    init(root, this.smallerValStack);
+```
+then pop the values.
+
+### Approach with memory limitation
+- constructor(): Push nodes to stack by walking left only, which will lead smallest at the top of stack
+- hasNext(): Just check stack
+- next():
+  - Pop the top of stack which is the return;
+  - Prepare for the next smallest by push the leftmost of right node if there is any.
+
+#FB #GOOGL #MSFT #LNKD
 
 #Tree #Stack
-#FB #GOOGL #MSFT #LNKD
--- p173m/bst_iterator.js
+++ p173m/bst_iterator.js
@@ -11,28 +11,30 @@ const Tree = require('binary_tree');
  * @param {TreeNode} root - root of the binary search tree
  */
 var BSTIterator = function(root) {
-    var init = function (root, stack) {
-        if (root === null) { return; }
-        init(root.right, stack);
-        stack.push(root.val);
-        init(root.left, stack);
+    this.pushLeft = function (p, stack) {
+        while (p !== null) {
+            stack.push(p);
+            p = p.left;
+        }
     };
-    this.smallerValStack = new Array();
-    init(root, this.smallerValStack);
+    this.stack = new Array();
+    this.pushLeft(root, this.stack);
 };
 /**
  * @this BSTIterator
  * @returns {boolean} - whether we have a next smallest number
  */
 BSTIterator.prototype.hasNext = function() {
-    return this.smallerValStack.length > 0;
+    return this.stack.length > 0;
 };
 /**
  * @this BSTIterator
  * @returns {number} - the next smallest number
  */
 BSTIterator.prototype.next = function() {
-    return this.smallerValStack.pop();
+    let top = this.stack.pop();
+    this.pushLeft(top.right, this.stack);
+    return top.val;
 };
 /**
  * Your BSTIterator will be called like this:
@@ -45,12 +47,10 @@ BSTIterator.prototype.next = function() {
     "1",
     "2,1,#,#,3,#,#",
     "5,3,2,1,#,#,#,4,#,#,7,6,#,#,8,#,9,#,#",
-].forEach(function (test) {
-    let tree = Tree.deserialize(test);
-    console.log("\n---");
-    console.log(tree);
+].forEach(t => {
+    let tree = Tree.deserialize(t);
     let i = new BSTIterator(tree);
     let a = [];
     while (i.hasNext()) a.push(i.next());
-    console.log("BSTIterator ->", a);
+    console.log("BSTIterator for", t, "->", a);
 });

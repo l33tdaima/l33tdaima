8c80e145dddd66cd3f67b09dcab36b486e74791d
l33tdaima <jwmiany@gmail.com>
2018-04-22 20:58:32 -0400

    Revisited p098m validate BST with a better solution

--- p098m/README.md
+++ p098m/README.md
@@ -7,15 +7,34 @@ Assume a BST is defined as follows:
 The left subtree of a node contains only nodes with keys less than the node's key.
 The right subtree of a node contains only nodes with keys greater than the node's key.
 Both the left and right subtrees must also be binary search trees.
-Example 1:
+### Example 1:
+```
     2
    / \
   1   3
+```
 Binary tree [2,1,3], return true.
-Example 2:
+### Example 2:
+```
     1
    / \
   2   3
+```
 Binary tree [1,2,3], return false.
+
+## Solution
+### Clarification
+Do we allow duplicates? No, if has duplicates, it is not a BST.
+
+### Range Check Approach
+A tree is BST if 
+1. its left is a BST and root is greater than any of left
+2. its right is a BST and root is less than any of the right
+Hence we can pass a min and max range into left and right child to validate, but the initial min and max boundary have to change for value type short, int, long, or double, which is quite cumbersome.
+
+### In-order Traversal Approach
+If we print the tree in-order, for BST tree it has to be an increasing sequence. This approach maintains a reference to the previous node in the sequence, and validate it must be less than the current value.
+
+#FB #MSFT #AMZN #BBG
 
 #Tree #Depth-first search
--- p098m/valid_bst.js
+++ p098m/valid_bst.js
@@ -10,7 +10,7 @@ const Tree = require('binary_tree');
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root) {
+var isValidBSTV1 = function(root) {
     // A tree is BST if 
     // 1. its left is a BST and root is greater than any of left
     // 2. its right is a BST and root is less than any of the right
@@ -22,8 +22,24 @@ var isValidBST = function(root) {
     };
     return recIsBSTHelper(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER); 
 };
-
-var testCases = [
+// The above solution relies on INT_MIN and INT_MAX which apparently doesn't scale to long, double etc.
+// This version traverses the tree in-order, checking if they form increasing order
+var isValidBSTV2 = function(root) {
+    let prev = null; // closure of the prev node, 
+                     // which must be less than the current node
+    let recValidate = function(node) {
+        if (node == null) { return true; }
+        if (!recValidate(node.left)) { return false; }
+        if (prev != null && prev.val != null && prev.val > node.val) {
+            return false;
+        }
+        prev = node;
+        return recValidate(node.right);
+    };
+    return recValidate(root);
+};
+// TEST
+testCases = [
     "0,#,#",
     "1,#,#",
     "1,2,#,#,#",
@@ -31,8 +47,8 @@ var testCases = [
     "2,1,#,#,#",
     "1,2,#,#,3,#,#",
     "2,1,#,#,3,#,#"
-];
-testCases.forEach(function(test) {
-    let t = Tree.deserialize(test);
-    console.log(test, "is a valid BST? ->", isValidBST(t));
+].forEach(t => {
+    let tree = Tree.deserialize(t);
+    console.log("Is", t, "a valid BST? ->", 
+                isValidBSTV1(tree), "&&", isValidBSTV2(tree));
 }, this);

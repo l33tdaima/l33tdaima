a8ca6bc21bcbea82170222e3b1dcd9f99e6f67ee
l33tdaima <jwmiany@gmail.com>
2018-04-29 15:57:02 -0400

    Revisited p033m search in rotated sorted array

--- p033m/README.md
+++ p033m/README.md
@@ -2,8 +2,36 @@
 
 Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
 
-(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+(i.e., [0 1 2 4 5 6 7] might become [4 5 6 7 0 1 2]).
 
 You are given a target value to search. If found in the array return its index, otherwise return -1.
 
 You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+### Example 1:
+```
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+```
+### Example 2:
+```
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+```
+
+## Solution
+Need to revise the standard binary search. 
+
+Between the part before mid and after mid, there must be one side is sorted, the other side is rotated,
+- nums[lo] <= nums[mid]: [lo .. mid] is sorted, [mid .. hi] is rotated
+  - target falls into [lo .. mid]: hi = mid - 1
+  - otherwise: lo = mid + 1
+- else: [lo .. mid] is rotated, [mid .. hi] is sorted
+  - target falls into [mid .. hi]: lo = mid + 1
+  - otherwise: hi = mid - 1
+
+#FB #MSFT #BBG #UBER #LNKD
+
+#Array #Binary Search
--- p033m/search_rotated.js
+++ p033m/search_rotated.js
@@ -4,96 +4,31 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    /**
-     * binary search helper closure with binded nums and target
-     * @param {number} start index
-     * @param {number} end index
-     * @return {number} index
-     */
-    if (nums.length === 0) {
-        return -1;
+    let lo = 0, hi = nums.length - 1;
+    while (lo < hi) {
+        let mid = ~~((lo + hi) / 2);
+        if (nums[mid] === target) { return mid; }
+        if (nums[lo] <= nums[mid]) { // [lo .. mid] is sorted, [mid .. hi] is not
+            if (target >= nums[lo] && target < nums[mid]) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        } else { // [mid .. hi] is sorted, [lo .. mid] is not
+            if (target <= nums[hi] && target > nums[mid]) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
     }
-
-    var bsearch = function(start, end) {
-        if (start < 0 || end >= nums.length) {
-            throw "Invalid start or end index";
-        }
-        if (start === end) {
-            return (nums[start] === target) ? start : -1;
-        }
-        // exact mid index if odd elements, low mid if even elements
-        let mid = ~~((end + start)/2); // integer quotient
-        if (mid === start) {
-            if(target === nums[mid]) {
-                return mid;
-            }
-            else {
-                return bsearch(mid + 1, end);
-            }
-        }
-        // You may assume no duplicate exists in the array
-        if (nums[start] === nums[end]) {
-            throw "Invalid array";
-        }
-        // Standard binary search
-        if (nums[start] < nums[end]) {
-            if (target === nums[mid]) {
-                return mid;
-            }
-            else if (target < nums[mid]) {
-                return bsearch(start, mid - 1);
-            }
-            else {
-                return bsearch(mid + 1, end);
-            }
-        }
-        // Rotated binary search
-        else { // (nums[start] > nums[end])
-            if (target === nums[mid]) {
-                return mid;
-            }
-            // a less than half low portion rotated to the right
-            if (nums[mid] > nums[start]) {
-                // no ambiguity, search target in the higher end
-                if (target > nums[mid]) {
-                    return bsearch(mid + 1, end);
-                }
-                // target could be lower end or higher end
-                else { // (target < nums[mid]
-                    if (target < nums[start]) {
-                        return bsearch(mid + 1, end);
-                    }
-                    else {
-                        return bsearch(start, mid - 1);
-                    }
-                }
-            }
-            // a less than half high portion rotated to the left
-            else { // (nums[mid] <= nums[start])
-                // no ambiguity, search target in the lower end
-                if (target < nums[mid]) {
-                    return bsearch(start, mid - 1);
-                }
-                // target could be lower or higher end
-                else {
-                    if (target > nums[end]) {
-                        return bsearch(start, mid - 1);
-                    }
-                    else {
-                        return bsearch(mid + 1, end);
-                    }
-                }
-
-            }
-        } // end of Rotated binary search 
-    };
-    return bsearch(0, nums.length - 1);
+    return (nums[lo] === target) ? lo : -1;
 };
-
-// var testArray = [ 5, 6, 7, 9, 0, 1, 2, 4];
-var testArray = [1, 3];
-var targets = [0, 3, 1, 4];
-console.log("Search in array: ", testArray);
-targets.forEach(function(val) {
-    console.log("Search " + val + ": index = " + search(testArray, val));
-}, this);
+// TEST
+[
+    [[4,5,6,7,0,1,2], 0],
+    [[4,5,6,7,0,1,2], 3],
+].forEach(t => {
+    console.log("Search", t[1], "in", t[0], "->",
+                search(t[0], t[1]));
+});

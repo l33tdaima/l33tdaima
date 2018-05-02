83a867d6c2b25cfcc2bd651e52d381f8e577ab5c
l33tdaima <jwmiany@gmail.com>
2018-05-01 23:24:50 -0400

    Reviewed p286m wall and gates

--- p286m/wall_and_gates.js
+++ p286m/wall_and_gates.js
@@ -3,14 +3,14 @@
  * @return {void} Do not return anything, modify rooms in-place instead.
  */
 var wallsAndGates = function(rooms) {
-    const WALL = 0, GATE = -1;
+    const WALL = -1, GATE = 0;
     const rows = rooms.length;
     if (rows === 0) { return; }
     cols = rooms[0].length;
     const queue = new Array();
     for (let i = 0; i < rows; ++i) {
         for (let j = 0; j < cols; ++j) {
-            if (rooms[i][j] === WALL) { queue.push([i, j]); }
+            if (rooms[i][j] === GATE) { queue.push([i, j]); }
         }
     }
     const dirs = [[-1, 0], [ 0, 1], [ 1, 0], [ 0,-1]];
@@ -21,7 +21,7 @@ var wallsAndGates = function(rooms) {
             let ni = i + d[0]; // next row index
             let nj = j + d[1]; // next col index
             if (ni >= 0 && ni < rows && nj >= 0 && nj < cols &&
-                rooms[ni][nj] !== GATE &&
+                rooms[ni][nj] !== WALL &&
                 rooms[i][j] + 1 < rooms[ni][nj]) {
                 rooms[ni][nj] = rooms[i][j] + 1;
                 queue.push([ni, nj]);

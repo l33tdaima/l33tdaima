530b6ef6e185984196ed16d0bfface7b5a1a9fda
l33tdaima <jwmiany@gmail.com>
2018-05-11 09:07:43 -0400

    Revisited p050m pow

--- p050m/README.md
+++ p050m/README.md
@@ -1,3 +1,34 @@
 # 50. Pow(x,n) (Medium)
 
-Implement pow(x, n).
+Implement pow(x, n). which calculates x raised to the power n.
+
+### Example 1:
+```
+Input: 2.00000, 10
+Output: 1024.00000
+```
+### Example 2:
+```
+Input: 2.10000, 3
+Output: 9.26100
+```
+### Example 3:
+```
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2^-2 = 1/2^2 = 1/4 = 0.25
+```
+### Note:
+```
+-100.0 < x < 100.0
+n is a 32-bit signed integer, within the range [−2^31, 2^31 − 1]
+```
+## Solution
+Solve with recursion with base case of n = 0, n = 1, then
+- negative n: 1/myPow(x, -n)
+- even n: myPow(x*x, n/2)
+- odd n: x * myPow(x*x, n/2)
+
+#GOOGL #FB #BBG #LNKD
+
+#Math #Binary Search
--- p050m/pow.js
+++ p050m/pow.js
@@ -4,27 +4,22 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-    if (n === 0) {
-        return 1;
-    }
-    if (n < 0) {
-        return 1/myPow(x, -n);
-    }
-    if (n === 1) {
-        return x;
-    }
-    if (n%2 === 0) {
-        let sqrt = myPow(x, n/2);
-        return sqrt * sqrt;
-    }
-    else {
-        let sqrt = myPow(x, ~~(n/2));
-        return x * sqrt * sqrt;
-    }
+    if (n === 0) { return 1; }
+    if (n === 1) { return x; }
+    if (n < 0) { return 1/myPow(x, -n); }
+    if (n%2 === 0) { return myPow(x*x, n/2); }
+    else { return x * myPow(x*x, ~~(n/2)); }
 };
-
-var x = parseFloat(process.argv[2]);
-var n = parseInt(process.argv[3]);
-
-console.log("Pow(", x, ",", n, ") =", myPow(x,n),
-            "Test passed?:", myPow(x,n) === Math.pow(x,n));
+// TEST
+[
+    [2.0, 10],
+    [2.1, 3],
+    [2.0, -2],
+    [0, 5],
+    [1, 100],
+    [23.123, 9],
+].forEach(t => {
+    let x = t[0], n = t[1];
+    console.log("Pow(" + x + ", " + n + ") =", myPow(x, n),
+                "Test passed?:", myPow(x,n) === Math.pow(x,n));
+});

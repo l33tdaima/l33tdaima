9c348ca8a3d89d3e759e9a7ff9d3f75e25fbcd40
l33tdaima <jwmiany@gmail.com>
2018-07-07 21:29:02 -0400

    Solved p293e flip game

---
+++ p293e/flip_game.cpp
@@ -0,0 +1,39 @@
+// g++ -std=c++11 *.cpp -o test && ./test && rm test
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> generatePossibleNextMoves(string s) {
+        vector<string> ans;
+        for (int i = 0; i < int(s.length() - 1); ++i) {
+            if (s[i] == '+' && s[i+1] == '+') {
+                ans.push_back(s.substr(0, i) + "--" + s.substr(i+2));
+            }
+        }
+        return ans;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    vector<string> tests = {
+        "",
+        "++++",
+        "+-+-",
+        "+-++++-",
+    };
+    Solution sol;
+    for (auto& t: tests) {
+        cout << "Possible next moves are -> ["; 
+        vector<string> actual = sol.generatePossibleNextMoves(t);
+        for (auto& s: actual) {
+            cout << s << ", ";
+        }
+        cout << "]" << endl;
+    }
+    return 0;
+}

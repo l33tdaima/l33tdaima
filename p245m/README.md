2bc90cc02f5a5cde12b343e2ad1b06130d47249a
l33tdaima <jwmiany@gmail.com>
2018-07-27 09:32:33 -0400

    Solved p245m shortest distance III

---
+++ p245m/README.md
@@ -0,0 +1,24 @@
+# 245. Shortest Word Distance III (Medium)
+
+Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+word1 and word2 may be the same and they represent two individual words in the list.
+
+### Example:
+```
+Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+Input: word1 = “makes”, word2 = “coding”
+Output: 1
+Input: word1 = "makes", word2 = "makes"
+Output: 3
+```
+### Note:
+You may assume word1 and word2 are both in the list.
+
+#LNKD
+
+#Array
+
+#Similar questions [#243e](../p243e/README.md) [#244m](../p244m/README.md) [#245m](../p245m/README.md)
+
--- p245m/shortest_distance3.js
+++ p245m/shortest_distance3.js
@@ -4,19 +4,22 @@
  * @param {string} word2
  * @return {number}
  */
-var shortestDistance = function(words, word1, word2) {
-    console.assert(word1 !== word2);
+var shortestWordDistance = function(words, word1, word2) {
     console.assert(words.length >= 2);
 
     let shortestDis = Number.MAX_SAFE_INTEGER;
     let iWord1 = -1, iWord2 = -1;
     for (let i = 0, len = words.length; i < len; ++i) {
-        if (words[i] === word1) {
-            iWord1 = i;
-        } else if (words[i] === word2) {
-            iWord2 = i;
-        } else {
+        if (words[i] !== word1 && words[i] !== word2) {
             continue;
+        }
+        if (words[i] === word1 && words[i] === word2) {
+            if (iWord1 <= iWord2) { iWord1 = i; }
+            else { iWord2 = i; }
+        } else if (words[i] === word1) {
+            iWord1 = i;
+        } else { // (words[i] === word2)
+            iWord2 = i;
         }
         if (iWord1 >= 0 && iWord2 >= 0) {
             shortestDis = Math.min(shortestDis, Math.abs(iWord1 - iWord2));
@@ -34,10 +37,14 @@ var shortestDistance = function(words, word1, word2) {
         words: ["practice", "makes", "perfect", "coding", "makes"],
         word1: "makes",
         word2: "coding"
+    }, {
+        words: ["practice", "makes", "perfect", "coding", "makes"],
+        word1: "makes",
+        word2: "makes"
     }
 ].forEach(function (test) {
     console.log("------");
     console.log(test.words);
     console.log("Shortest distance from '" + test.word1 + "' to '" + test.word2 + "' ->",
-                shortestDistance(test.words, test.word1, test.word2));
+                shortestWordDistance(test.words, test.word1, test.word2));
 });

5d002a957f2070dde9127b6a0c4a31adbab92833
l33tdaima <jwmiany@gmail.com>
2018-12-08 18:01:33 -0500

    Solved p349e intersection with help on optimization

---
+++ p349e/intersection.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+  let set1 = new Set(nums1);
+  return [...new Set(nums2.filter(n => set.has(n)))];
+};
+// TESTS
+[
+  [[], []],
+  [[1], [2]],
+  [[1, 2, 2], [3]],
+  [[1, 2, 2, 1], [2, 2]],
+  [[4, 9, 5], [9, 4, 9, 8, 4]]
+].forEach(t => {
+  console.log(
+    "Intersection of",
+    t[0],
+    "and",
+    t[1],
+    "->",
+    intersection(t[0], t[1])
+  );
+});

86c60aa7c60cddbe8e975ffc5649e6a4444d03c1
l33tdaima <jwmiany@gmail.com>
2018-12-09 14:49:57 -0500

    Solved p438e find anagram with help

---
+++ p438e/find_anagrams.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+var findAnagrams = function(s, p) {
+  let ans = [];
+  if (s.length === 0 || p.length === 0 || s.length < p.length) return ans;
+  // create char occurrence hash table from p
+  let hash = Array.from({ length: 256 }, v => 0);
+  for (let c of p) hash[c.charCodeAt(0)]++;
+
+  let [left, right, count] = [0, 0, p.length];
+  while (right < s.length) {
+    // decrement occurence when moving right side of sliding window
+    if (hash[s.charCodeAt(right++)]-- >= 1) count--;
+    if (count === 0) ans.push(left);
+    // increment (reset) occurence when moving left side of window
+    if (right - left === p.length && hash[s.charCodeAt(left++)]++ >= 0) count++;
+  }
+  return ans;
+};
+// TESTS
+[
+  {
+    s: "cbaebabacd",
+    p: "abc",
+    expected: [0, 6]
+  },
+  {
+    s: "abab",
+    p: "abb",
+    expected: [1]
+  },
+  {
+    s: "abab",
+    p: "ab",
+    expected: [0, 1, 2]
+  }
+].forEach(t => {
+  let actual = findAnagrams(t.s, t.p);
+  console.log("Find anagram in", t.s, "of", t.p, "->", actual);
+  //console.assert(actual === t.expected);
+});

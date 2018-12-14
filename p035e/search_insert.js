6ea222772510cd70ad7fcaaf09f9bf28cb847954
l33tdaima <jwmiany@gmail.com>
2018-12-14 09:24:16 -0500

    Solved p035e search insert beats 100%

---
+++ p035e/search_insert.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  let [left, right] = [0, nums.length];
+  // The two lines before are just added to speed up,
+  // without them algorithm are still accurate and elegant
+  if (target < nums[0]) return left;
+  if (target > nums[right - 1]) return right;
+
+  while (left < right) {
+    let mid = ~~((left + right) / 2);
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] < target) left = mid + 1;
+    else right = mid;
+  }
+  return right;
+};
+// TESTS
+[
+  {
+    nums: [1, 3, 5, 6],
+    target: 5,
+    expected: 2
+  },
+  {
+    nums: [1, 3, 5, 6],
+    target: 2,
+    expected: 1
+  },
+  {
+    nums: [1, 3, 5, 6],
+    target: 7,
+    expected: 4
+  },
+  {
+    nums: [1, 3, 5, 6],
+    target: 0,
+    expected: 0
+  }
+].forEach(t => {
+  let actual = searchInsert(t.nums, t.target);
+  console.log("Search insert in", t.nums, "for", t.target, "->", actual);
+  console.assert(actual === t.expected);
+});

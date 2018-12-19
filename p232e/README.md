b8602ba8ceb35afc7e9685e3cf1122383bdb9b82
l33tdaima <jwmiany@gmail.com>
2018-12-19 09:07:55 -0500

    Solved p232e implement queue using stack with hints

---
+++ p232e/README.md
@@ -0,0 +1,32 @@
+# 232. Implement Queue using Stacks (Easy)
+
+Implement the following operations of a queue using stacks.
+
+- push(x) -- Push element x to the back of queue.
+- pop() -- Removes the element from in front of queue.
+- peek() -- Get the front element.
+- empty() -- Return whether the queue is empty.
+
+## Example:
+```
+MyQueue queue = new MyQueue();
+
+queue.push(1);
+queue.push(2);  
+queue.peek();  // returns 1
+queue.pop();   // returns 1
+queue.empty(); // returns false
+```
+
+### Notes:
+
+- You must use only standard operations of a stack -- which means only push to top, peek/pop from top, size, and is empty operations are valid.
+- Depending on your language, stack may not be supported natively. You may simulate a stack by using a list or deque (double-ended queue), as long as you use only standard operations of a stack.
+- You may assume that all operations are valid (for example, no pop or peek operations will be called on an empty queue).
+
+## Solution
+We will have one input stack, onto which we push the incoming elements, and one output stack, from which we peek/pop. We only move elements from input stack to output stack when needed, i.e., when I need to peek/pop but the output stack is empty. When that happens, I move all elements from input to output stack, thereby reversing the order so it's the correct order for peek/pop.
+
+#MSFT #GOOGL
+
+#Stack #Design
---
+++ p232e/my_queue.cpp
@@ -0,0 +1,65 @@
+// g++ -std=c++11 *.cpp -o test && ./test && rm -f test
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+class MyQueue {
+    stack<int> in;
+    stack<int> out;
+
+    void transfer() {
+        while (!in.empty()) {
+            out.push(in.top());
+            in.pop();
+        }
+    }
+public:
+    /** Initialize your data structure here. */
+    MyQueue()
+    : in(), out() {
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        in.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        if (out.empty()) { transfer(); }
+        int ret = out.top();
+        out.pop();
+        return ret;
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        if (out.empty()) { transfer(); }
+        return out.top();
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return in.empty() && out.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * bool param_4 = obj.empty();
+ */
+int main(int argc, char const *argv[])
+{
+    MyQueue queue;
+    queue.push(1);
+    queue.push(2);
+    assert(queue.peek() == 1);
+    assert(queue.pop() == 1);
+    assert(queue.empty() == false);
+    return 0;
+}

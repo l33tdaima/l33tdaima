e2d496f907d1748dae7950f37589c2f2b7467efb
l33tdaima <jwmiany@gmail.com>
2018-12-22 17:21:28 -0500

    Solved p905e sort array by parity

---
+++ p905e/sort_by_parity.js
@@ -0,0 +1,56 @@
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+
+var sortArrayByParityOutPlace = function(A) {
+  let ans = new Array(A.length);
+  let le = 0,
+    ro = A.length - 1;
+  for (let a of A) {
+    if (a % 2 === 0) ans[le++] = a;
+    else ans[ro--] = a;
+  }
+  return ans;
+};
+
+var sortArrayByParityFunctional = function(A) {
+  return A.reduce((ans, a) => {
+    if (a % 2 === 0) ans.unshift(a);
+    else ans.push(a);
+    return ans;
+  }, []);
+};
+
+var sortArrayByParityInPlace = function(A) {
+  let le = 0,
+    ro = A.length - 1;
+  while (le < ro) {
+    if (A[le] % 2 === 0) le++;
+    else if (A[ro] % 2 !== 0) ro--;
+    else if (le < ro && le < A.length && ro > 0) {
+      [A[le], A[ro]] = [A[ro], A[le]];
+    }
+  }
+  return A;
+};
+// TESTS
+[
+  [],
+  [1, 2],
+  [1, 3],
+  [2, 4],
+  [3, 1, 2, 4],
+  [1, 2, 3, 4, 5],
+  [1, 2, 3, 4, 5, 6, 7, 8]
+].forEach(t => {
+  let orig = Array.from(t);
+  console.log(
+    "Sort",
+    orig,
+    "by parity ->",
+    sortArrayByParityFunctional(t),
+    sortArrayByParityOutPlace(t),
+    sortArrayByParityInPlace(t)
+  );
+});

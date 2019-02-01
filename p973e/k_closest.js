e1bff8e11c6d93aadb568cd00f49339dec6edd99
l33tdaima <jwmiany@gmail.com>
2019-02-01 09:43:42 -0500

    Solved p973e K cloest points to origin with help on O(N) solution

---
+++ p973e/k_closest.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[][]} points
+ * @param {number} K
+ * @return {number[][]}
+ */
+let compare = (p1, p2) =>
+  p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1];
+
+let helper = (points, l, r) => {
+  let pivot = Array.from(points[l]);
+  while (l < r) {
+    while (l < r && compare(points[r], pivot) >= 0) r--;
+    points[l] = points[r];
+    while (l < r && compare(points[l], pivot) <= 0) l++;
+    points[r] = points[l];
+  }
+  points[l] = [...pivot];
+  return l;
+};
+
+var kClosest = function(points, K) {
+  let [l, r] = [0, points.length - 1];
+  while (l < r) {
+    let mid = helper(points, l, r);
+    if (mid === K) break;
+    if (mid < K) l = mid + 1;
+    else r = mid - 1;
+  }
+  return points.slice(0, K);
+};
+// TESTS
+[
+  {
+    points: [[1, 3], [-2, 2]],
+    K: 1
+  },
+  {
+    points: [[3, 3], [5, -1], [-2, 4]],
+    K: 2
+  }
+].forEach(t => {
+  console.log("K closet point in", t.points, "->", kClosest(t.points, t.K));
+});

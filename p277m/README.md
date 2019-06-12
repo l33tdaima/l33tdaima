9dde683ac343854cdaecaa028c5aae96023ecdc5
l33tdaima <jwmiany@gmail.com>
2019-06-12 15:29:21 -0400

    Revisited p277m find celebrity

--- p277m/README.md
+++ p277m/README.md
@@ -4,9 +4,33 @@ Suppose you are at a party with n people (labeled from 0 to n - 1) and among the
 
 Now you want to find out who the celebrity is or verify that there is not one. The only thing you are allowed to do is to ask questions like: "Hi, A. Do you know B?" to get information of whether A knows B. You need to find out the celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
 
-You are given a helper function bool knows(a, b) which tells you whether A knows B. Implement a function int findCelebrity(n), your function should minimize the number of calls to knows.
+You are given a helper function `bool knows(a, b)` which tells you whether A knows B. Implement a function `int findCelebrity(n)`, your function should minimize the number of calls to knows.
 
 Note: There will be exactly one celebrity if he/she is in the party. Return the celebrity's label if there is a celebrity in the party. If there is no celebrity, return -1.
+
+### Example 1:
+```
+Input: graph = [
+  [1,1,0],
+  [0,1,0],
+  [1,1,1]
+]
+Output: 1
+Explanation: There are three persons labeled with 0, 1 and 2. graph[i][j] = 1 means person i knows person j, otherwise graph[i][j] = 0 means person i does not know person j. The celebrity is the person labeled as 1 because both 0 and 2 know him but 1 does not know anybody.
+```
+### Example 2:
+```
+Input: graph = [
+  [1,0,1],
+  [1,1,0],
+  [0,1,1]
+]
+Output: -1
+Explanation: There is no celebrity.
+```
+### Note:
+1. The directed graph is represented as an adjacency matrix, which is an n x n matrix where a[i][j] = 1 means person i knows person j while a[i][j] = 0 means the contrary.
+2. Remember that you won't have direct access to the adjacency matrix.
 
 ## Solution
 The first loop is to find the candidate by following, if candidate knows i, then switch candidate. In detail, suppose the candidate after the first for loop is person k, it means 0 to k-1 cannot be the celebrity, because they know a previous or current candidate, or they are unknown to a previous candidate. Also, since k knows no one between k+1 and n-1, k+1 to n-1 can not be the celebrity either. Therefore, k is the only possible celebrity, if there exists one.
@@ -18,3 +42,5 @@ To this point, I actually realize that we can further shrink the calling of know
 #LNKD #FB
 
 #Array
+
+#Similar questions [#277](../p277m/README.md) [#997](../p997e/README.md)
--- p277m/find_celebrity.js
+++ p277m/find_celebrity.js
@@ -1,6 +1,6 @@
 /**
  * Definition for knows()
- * 
+ *
  * @param {integer} person a
  * @param {integer} person b
  * @return {boolean} whether a knows b
@@ -11,64 +11,42 @@
  * @param {function} knows()
  * @return {function}
  */
-var brutalForce = false;
 var solution = function(knows) {
-    /**
-     * @param {integer} n Total people
-     * @return {integer} The celebrity
-     */
-  if (brutalForce) {
-    // brutal force with early termination
-    return function(n) {
-        for (let i = 0; i < n; ++i) {
-            // is i a celebrity because of she knows no one?
-            let j = 0;
-            while (j < n) {
-                if (i !== j && knows(i, j)) {
-                    break;
-                }
-                j++;
-            }
-            if (j < n) { continue; }
-            // test if she is known by all
-            for (j = 0; j < n; ++j) {
-                if (i !== j && !knows(j, i)) {
-                    break;
-                }
-            }
-            if (j === n) { return i; }
-        }
+  /**
+   * @param {integer} n Total people
+   * @return {integer} The celebrity
+   */
+  // optimized solution
+  return function(n) {
+    // find the candidate in the first pass
+    let candidate = 0;
+    for (let i = 1; i < n; ++i) {
+      if (knows(candidate, i)) candidate = i;
+    }
+    for (let i = 0; i < n; ++i) {
+      if ((i < candidate && knows(candidate, i)) || !knows(i, candidate))
         return -1;
-    };
-  } else {
-    // optimized solution
-    return function (n) {
-        // find the candidate in the first pass
-        let candidate = 0;
-        for (let i = 1; i < n; ++i) {
-            if (knows(candidate, i)) {
-                candidate = i;
-            }
-        }
-        for (let i = 0; i < n; ++i) {
-            if (i < candidate && knows(candidate, i) || !knows(i, candidate)) return -1;
-            if (i > candidate && !knows(i, candidate)) return -1;
-        }
-        return candidate;
-    };
-  }
+      if (i > candidate && !knows(i, candidate)) return -1;
+    }
+    return candidate;
+  };
 };
-// TEST
+// TESTS
 [
-    2,
-    3,
-    10,
-].forEach(function (test) {
-    let knows = function(a, b) {
-        if (b === test - 1) return true;
-        if (a === test - 1) return false;
-        return true;
-    };
-    console.log("Find the celebrity in", test, "->",
-                solution(knows)(test));
+  {
+    graph: [[1, 1, 0], [0, 1, 0], [1, 1, 1]],
+    expected: 1
+  },
+  {
+    graph: [[1, 0, 1], [1, 1, 0], [0, 1, 1]],
+    expected: -1
+  }
+].forEach(t => {
+  const knows = (a, b) => {
+    return t.graph[a][b] === 1;
+  };
+  const n = t.graph.length;
+  const actual = solution(knows)(n);
+  console.log('Find the celebrity in', n, 'people ->', actual);
+  console.assert(actual === t.expected);
 });

2652735f3a9dd9aea459f8277f3f040c7d54381f
l33tdaima <jwmiany@gmail.com>
2019-08-26 09:13:14 -0400

    Revisited p002m add two numbers

---
+++ p002m/add_two_numbers.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode sentinel(0);
+        ListNode* p = &sentinel;
+        div_t sum = {0, 0};
+        while (sum.quot > 0 || l1 || l2) {
+            if (l1) {
+                sum.quot += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum.quot += l2->val;
+                l2 = l2->next;
+            }
+            sum = div(sum.quot, 10);
+            p->next = new ListNode(sum.rem);
+            p = p->next;
+        }
+        return sentinel.next;
+    }
+};
--- p002m/add_two_numbers.js
+++ p002m/add_two_numbers.js
@@ -12,37 +12,37 @@ const List = require('list');
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    if (l1 === null && l2 === null) {
-        return null;
-    }
-    let head = new List.ListNode(0);
-    let p = head;
-    let carry = 0;
-    while (l1 !== null || l2 !== null) {
-        let s = (l1?l1.val:0) + (l2?l2.val:0) + carry;
-        carry = ~~(s/10); // integer quotient
-        p.next = new List.ListNode(s%10);
-        p = p.next;
-        if (l1 !== null) { l1 = l1.next; }
-        if (l2 !== null) { l2 = l2.next; }
-    }
-    if (carry > 0) {
-        p.next = new List.ListNode(carry);
-    }
-    return head.next;
+  if (l1 == null && l2 == null) return null;
+  let head = new List.ListNode(0); // dummy head
+  let p = head;
+  let carry = 0;
+  while (carry > 0 || l1 !== null || l2 !== null) {
+    let s = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+    carry = ~~(s / 10); // integer quotient
+    p.next = new List.ListNode(s % 10);
+    p = p.next;
+    if (l1 !== null) l1 = l1.next;
+    if (l2 !== null) l2 = l2.next;
+  }
+  return head.next;
 };
 // TEST
 [
-    [[],[]],
-    [[1],[]],
-    [[3],[5]],
-    [[6],[9]],
-    [[6,5],[9,0,2]],
-    [[6,5],[8,4,9]],
-    [[2,4,3],[5,6,4]],
-].forEach(function(test) {
-    let l0 = List.fromArray(test[0]);
-    let l1 = List.fromArray(test[1]);
-    console.log(test[0].toString(), "+", test[1].toString(),
-                "=", List.toArray(addTwoNumbers(l0, l1)).toString());
+  [[], []],
+  [[1], []],
+  [[3], [5]],
+  [[6], [9]],
+  [[6, 5], [9, 0, 2]],
+  [[6, 5], [8, 4, 9]],
+  [[2, 4, 3], [5, 6, 4]]
+].forEach(t => {
+  let l0 = List.fromArray(t[0]);
+  let l1 = List.fromArray(t[1]);
+  console.log(
+    t[0].toString(),
+    '+',
+    t[1].toString(),
+    '=',
+    List.toArray(addTwoNumbers(l0, l1)).toString()
+  );
 });

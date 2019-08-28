afb39dca612fa61a906772332d75f1317754b2db
l33tdaima <jwmiany@gmail.com>
2019-08-27 20:49:57 -0400

    Revisited p138m copy random list

--- p138m/README.md
+++ p138m/README.md
@@ -4,6 +4,19 @@ A linked list is given such that each node contains an additional random pointer
 
 Return a deep copy of the list.
 
+### Example
+```
+Input:
+{"$id":"1","next":{"$id":"2","next":null,"random":{"$ref":"2"},"val":2},"random":{"$ref":"2"},"val":1}
+
+Explanation:
+Node 1's value is 1, both of its next and random pointer points to Node 2.
+Node 2's value is 2, its next pointer points to null and its random pointer points to itself.
+```
+
+### Note:
+You must return the copy of the given head as a reference to the cloned list.
+
 ## Solution
 The algorithm is composed of the follow three steps which are also 3 iteration rounds.
 
@@ -13,5 +26,10 @@ The algorithm is composed of the follow three steps which are also 3 iteration r
 
 O(n) in time and space complexity, no extra space needed.
 
-#AMZN #MSFT #BBG #UBER
 #Linked List #Hash Table
+
+#AMZN #MSFT #FB #BBG #GOOGL
+
+#Explore Facebook
+
+#Similar questions [#133](../p133m/README.md) [#138](../p138m/README.md)
--- p138m/copy_random_list.js
+++ p138m/copy_random_list.js
@@ -1,72 +1,70 @@
 /**
  * Definition for singly-linked list with a random pointer.
  */
-function RandomListNode(label) {
-    this.label = label;
-    this.next = this.random = null;
+function Node(val, next, random) {
+  this.val = val;
+  this.next = next;
+  this.random = random;
 }
 
 /**
- * @param {RandomListNode} head
- * @return {RandomListNode}
+ * @param {Node} head
+ * @return {Node}
  */
 var copyRandomList = function(head) {
-    if (head == null) { return head; }
-    // Round 1: copy the node and insert it right behind
-    let it = head;
-    while (it !== null) {
-        let next = it.next;
-        let cp = new RandomListNode(it.label);
-        it.next = cp;
-        cp.next = next;
-        it = next;
-    }
-    // Round 2: copy the random link
-    it = head;
-    while (it !== null) {
-        if (it.random !== null) {
-            it.next.random = it.random.next;
-        }
-        it = it.next.next;
-    }
-    // Round 2: detach the copy
-    it = head;
-    let dummyHeadCopy = new RandomListNode(0);
-    let p = dummyHeadCopy;
-    while (it !== null) {
-        let next = it.next.next;
-        // extract the copy
-        let copy = it.next;
-        p.next = copy;
-        p = copy;
-        // restore the original
-        it.next = next;
-        it = next;
-    }
-    return dummyHeadCopy.next;
+  if (head == null) return null;
+  // Round 1: copy the node and insert it right behind
+  let it = head;
+  while (it !== null) {
+    let next = it.next;
+    let cp = new Node(it.val, next, null);
+    it.next = cp;
+    it = next;
+  }
+  // Round 2: copy the random link
+  it = head;
+  while (it !== null) {
+    if (it.random !== null) it.next.random = it.random.next;
+    it = it.next.next;
+  }
+  // Round 3: detach the copy
+  it = head;
+  let sentinel = new Node(0, null, null);
+  let p = sentinel;
+  while (it !== null) {
+    let next = it.next.next;
+    // extract the copy
+    let copy = it.next;
+    p.next = copy;
+    p = copy;
+    // restore the original
+    it.next = next;
+    it = next;
+  }
+  return sentinel.next;
 };
 // TEST
-let dummyHead = new RandomListNode(null);
+let dummyHead = new Node(0, null, null);
 let hashTable = {};
 let p = dummyHead;
 let test = ['a', 'b', 'c', 'd', 'e'];
-test.forEach(function (label) {
-    let node = new RandomListNode(label);
-    hashTable[label] = node;
-    p.next = node;
-    p = p.next;
+test.forEach(function(label) {
+  let node = new Node(label, null, null);
+  hashTable[label] = node;
+  p.next = node;
+  p = p.next;
 });
 p = dummyHead.next;
 while (p !== null) {
-    let label = test[~~(Math.random() * (~~test.length))];
-    p.random = hashTable[label];
-    console.log("node:", p.label + ", p.random =", label, "->");
-    p = p.next;
+  let label = test[~~(Math.random() * test.length)];
+  p.random = hashTable[label];
+  console.log('node:', p.val + ', p.random =', label, '->');
+  p = p.next;
 }
-console.log("Deep copy random list");
+console.log('Deep copy random list');
 let copy = copyRandomList(dummyHead.next);
 let pc = copy;
 while (pc !== null) {
-    console.log("node:", pc.label + ", pc.random =", pc.random.label, "->");
-    pc = pc.next;
+  console.log('node:', pc.val + ', pc.random =', pc.random.val, '->');
+  pc = pc.next;
 }

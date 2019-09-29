e5b8d5f5aaf66f8eb889f7b8cd218f70b24ac30f
Mianyu Wang <mwang57@bloomberg.net>
2019-09-28 21:30:06 -0400

    Revisited p001e two sum

--- p001e/two_sum.cpp
+++ p001e/two_sum.cpp
@@ -1,10 +1,8 @@
 // Build program by
 // g++ -std=c++11 *.cpp -o test && ./test && rm test
-
 #include <vector>
 #include <unordered_map>
 
-#include <initializer_list>
 #include <iostream>
 
 using namespace std;
@@ -15,14 +13,11 @@ public:
         unordered_map<int, int> ht;
         for (int i = 0; i < nums.size(); ++i) {
             int key = target - nums[i];
-            if (ht.find(key) != ht.end()) {
-                vector<int> res;
-                res.push_back(ht[key]);
-                res.push_back(i);
+            if (ht.count(key) != 0) {
+                vector<int> res = {ht[key], i};
                 return res;
-            } else {
-                ht[nums[i]] = i;
             }
+            ht[nums[i]] = i;
         }
         return vector<int>();
     }
@@ -30,28 +25,27 @@ public:
 
 // TEST
 struct Test {
-    initializer_list<int> numsIList;
+    vector<int> nums;
     int target;
     
     void run() {
-        vector<int> nums(numsIList);
         Solution sol;
-        auto res = sol.twoSum(nums, target);
+        auto actual = sol.twoSum(nums, target);
         cout << "Two sum of [";
         for (auto n : nums) { cout << n << ","; }
-        cout << "] target " << target << " -> (";
-        for (auto i : res) { cout << i << ","; }
-        cout << ")" << endl;
+        cout << "] target " << target << " -> [";
+        for (auto i : actual) { cout << i << ","; }
+        cout << "]" << endl;
     }
 };
-// g++ -std=c++11 two_sum.cpp -o test && ./test && rm test
+
 int main(int argc, char *argv[]) {
-    initializer_list<Test> tests = {
+    vector<Test> tests = {
         {{3,2,4}, 6},
         {{2,1,3}, 3},
         {{3,2,1,5,6,4}, 10},
         {{3,2,1,5,6,4}, 6},
     };
-    for (auto t : tests) { t.run(); }
+    for (auto t : tests) t.run();
     return 0;
 }
--- p001e/two_sum.js
+++ p001e/two_sum.js
@@ -4,30 +4,22 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    var map = {};
-    for(var i = 0; i < nums.length; ++i) {
-        var complement = target - nums[i];
-        if (map[complement] !== undefined) {
-            return [map[complement], i];
-        }
-        map[nums[i]] = i;
-    }
-    return [];
-}
-
-var testCases = [3, 2, 4];
-var target = 6;
-
-var result = twoSum(testCases, target);
-if(result.length > 0)
-{
-    console.log("Two numbers sum up to " + target + 
-                " are: " + result[0] + ", " + result[1]);
-    console.log(testCases[result[0]] +
-                " and " +
-                testCases[result[1]]);
-}
-else
-{
-    console.log("Not found");
-}
+  const map = new Map();
+  for (let i = 0; i < nums.length; ++i) {
+    let complement = target - nums[i];
+    if (map.has(complement)) return [map.get(complement), i];
+    map.set(nums[i], i);
+  }
+  return [];
+};
+// TESTS
+[
+  [[], 9],
+  [[3, 2, 4], 4],
+  [[3, 2, 4], 6],
+  [[2, 7, 11, 15], 9],
+  [[1, 2, 3, 5, 6, 7], 15]
+].forEach(t => {
+  const actual = twoSum(t[0], t[1]);
+  console.log('Two sum in', t[0], 'of', t[1], '->', actual);
+});

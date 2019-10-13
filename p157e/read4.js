3c4087ad1bb40512710f066f8473260168fc0365
l33tdaima <jwmiany@gmail.com>
2019-10-13 15:45:25 -0400

    Revisited p157e read 4 characters given read4

--- p157e/read4.js
+++ p157e/read4.js
@@ -1,17 +1,17 @@
 /**
  * Definition for read4()
- * 
+ *
  * @param {character[]} buf Destination buffer
  * @return {number} The number of characters read
  */
-var file = "123456789".split("");
-var p = 0;
-var read4 = function(buf) {
-    let i = 0;
-    while (p < file.length && i < 4) {
-        buf[i++] = file[p++];
-    }
-    return i;
+const file = '123456789'.split('');
+let p = 0;
+const read4 = function(buf) {
+  let i = 0;
+  while (p < file.length && i < 4) {
+    buf[i++] = file[p++];
+  }
+  return i;
 };
 
 /**
@@ -19,33 +19,37 @@ var read4 = function(buf) {
  * @return {function}
  */
 var solution = function(read4) {
-    /**
-     * @param {character[]} buf Destination buffer
-     * @param {number} n Maximum number of characters to read
-     * @return {number} The number of characters read
-     */
-    return function(buf, n) {
-        let total = 0, eof = false;
-        let buf4 = [];
-        while (!eof && total < n) {
-            let nc = read4(buf4);
-            eof = (nc < 4);
-            nc = Math.min(nc, n - total);
-            total += nc;
-            for (let i = 0; i < nc; ++i) {
-                buf.push(buf4[i]);
-            }
-        }
-        return total;
-    };
+  /**
+   * @param {character[]} buf Destination buffer
+   * @param {number} n Maximum number of characters to read
+   * @return {number} The number of characters read
+   */
+  return function(buf, n) {
+    let [total, eof] = [0, false];
+    let buf4 = [];
+    while (!eof && total < n) {
+      let nc = read4(buf4);
+      eof = nc < 4;
+      nc = Math.min(nc, n - total);
+      total += nc;
+      for (let i = 0; i < nc; ++i) {
+        buf.push(buf4[i]);
+      }
+    }
+    return total;
+  };
 };
 // TEST
-[
-    0,1,2,3,4,5,6,7,8,9,10,
-].forEach(t => {
-    let buf = [];
-    console.log("Read", t, "chars from file content", file, "->",
-                solution(read4)(buf, t));
-    console.log("Buffer ->", buf);
-    p = 0; // reset
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].forEach(t => {
+  let buf = [];
+  console.log(
+    'Read',
+    t,
+    'chars from file content',
+    file,
+    '->',
+    solution(read4)(buf, t)
+  );
+  console.log('Buffer ->', buf);
+  p = 0; // reset
 });

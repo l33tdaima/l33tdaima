a1ba21ad54049fbd492a64f456c8d899607d4326
l33tdaima <jwmiany@gmail.com>
2020-01-17 09:44:13 -0500

    Solved p081m search in rotated array II in Python with help

--- p081m/search_rotated2.py
+++ p081m/search_rotated2.py
@@ -2,34 +2,36 @@ from typing import List
 
 
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def search(self, nums: List[int], target: int) -> bool:
         lo, hi = 0, len(nums) - 1
         while lo <= hi:
             mid = lo + (hi - lo) // 2
-            if nums[mid] == target:
-                return mid
-            if nums[lo] <= nums[mid]:  # left is sorted
+            if target == nums[mid]:
+                return True
+            # tricky part to filter the duplicates
+            if nums[lo] == nums[mid] == nums[hi]:
+                lo, hi = lo + 1, hi - 1
+            elif nums[lo] <= nums[mid]:  # left is absolutely sorted
                 if nums[lo] <= target < nums[mid]:
                     hi = mid - 1
                 else:
                     lo = mid + 1
-            else:  # right is sorted
+            else:
                 if nums[mid] < target <= nums[hi]:
                     lo = mid + 1
                 else:
                     hi = mid - 1
-        return -1
+        return False
 
 
 # TESTS
 tests = [
-    [[3, 1], 1, 1],
-    [[4, 5, 6, 7, 0, 1, 2], 0, 4],
-    [[4, 5, 6, 7, 0, 1, 2], 3, -1],
-    [[5, 6, 0, 1, 2, 3, 4], 5, 0],
-    [[5, 6, 0, 1, 2, 3, 4], 4, 6],
-    [[4, 5, 6, 7, 0, 1, 2, 3], 3, 7],
-    [[3, 4, 5, 6, 7, 0, 1, 2], 0, 5],
+    [[0, 5, 6, 0, 0, 0], 5, True],
+    [[0, 5, 6, 0, 0, 0], 6, True],
+    [[2, 5, 6, 0, 0, 1, 2], 0, True],
+    [[2, 5, 6, 0, 0, 1, 2], 3, False],
+    [[0, 5, 6, 0, 0, 0, 0], 6, True],
+    [[0, 0, 0, 0, 0, 0, 0], 5, False],
 ]
 for t in tests:
     sol = Solution()

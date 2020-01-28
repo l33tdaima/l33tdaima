a3221fe0a19c33655aeaf095d3d5319f5f6aa63c
l33tdaima <jwmiany@gmail.com>
2020-01-28 08:51:31 -0500

    Reviewed p162m find peak element

--- p162m/find_peak_element.js
+++ p162m/find_peak_element.js
@@ -3,26 +3,26 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    let len = nums.length;
-    let l = 0, r = len - 1;
-    while (l < r) {
-        let m = ~~((r - l)/2) + l; // protect sum overflow
-        if (nums[m] < nums[m + 1]) {
-            l = m + 1;
-        } else { // nums[m] > num[m + 1]
-            r = m;
-        }
+  let [l, r] = [0, nums.length - 1];
+  while (l < r) {
+    let m = ~~((r - l) / 2) + l;
+    if (nums[m] < nums[m + 1]) {
+      l = m + 1;
+    } else {
+      r = m;
     }
-    return l;
+  }
+  return l;
 };
 // TEST
 [
-    //[1],
-    [1, 2],
-    [2, 1],
-    [1, 2, 1],
-    [2, 1, 2],
-    [1, 2, 3, 1]
+  [[1, 2], 1],
+  [[2, 1], 0],
+  [[1, 2, 1], 1],
+  [[2, 1, 2], 2],
+  [[1, 2, 3, 1], 2]
 ].forEach(t => {
-    console.log("Any peak of", t, "->", findPeakElement(t));
+  const actual = findPeakElement(t[0]);
+  console.log('Any peak of', t[0], '->', actual);
+  console.assert(t[1] === actual);
 });

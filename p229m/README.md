6a35618410a62b9b069fa319fc6cbfe5271a96ca
l33tdaima <jwmiany@gmail.com>
2020-03-04 09:29:49 -0500

    Revisited p229 majority element II

--- p229m/README.md
+++ p229m/README.md
@@ -1,8 +1,24 @@
 # 229. Majority Element II (Medium)
 
-Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times. The algorithm should run in linear time and in O(1) space.
+Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times. 
+
+*Note*: The algorithm should run in linear time and in O(1) space.
+
+### Example 1:
+```
+Input: [3,2,3]
+Output: [3]
+```
+
+### Example 2:
+```
+Input: [1,1,1,3,3,2,2,2]
+Output: [1,2]
+```
 
 ## Solution
-There will be no more than 2 elements in the outcome. A variant of Boyer-Moore Algorithm which we must check candidate 1 (m1) and candidate 2 (m2) first, not zero first as we did in p169e, otherwise we could have m1 and m2 the same in some test cases.
+Boyer-Moore Voting Algorithm is used to solve the problem of find the the candidates with more than half votes from arbitrary candidates. For the first round of eliminating, compare the votes pair, cancel out if they are different, add the votes for the candidate if they are the same, and compare with the next, and so on, until finding a possible candidate. For the second counting round, count his actual votes to see if he is really the winner.
+
+This problem is the variant of this algorithm. There will be no more than 2 elements in the outcome. A variant of Boyer-Moore Algorithm which we must check candidate 1 (m1) and candidate 2 (m2) first, not zero first as we did in p169e, otherwise we could have m1 and m2 the same in some test cases.
 
 #Array
--- p229m/majority_element2.js
+++ p229m/majority_element2.js
@@ -3,47 +3,61 @@
  * @return {number[]}
  */
 var majorityElement = function(nums) {
-    if (nums == null || nums.length === 0) {
-        return [];
+  if (nums == null || nums.length === 0) return [];
+  let [m1, m2] = [null, null];
+  let [c1, c2] = [0, 0];
+  // Eliminating
+  for (let n of nums) {
+    if (n === m1) {
+      c1++;
+    } else if (n === m2) {
+      c2++;
+    } else if (c1 === 0) {
+      m1 = n;
+      c1 = 1;
+    } else if (c2 === 0) {
+      m2 = n;
+      c2 = 1;
+    } else {
+      c1--;
+      c2--;
     }
-    let m1 = null, m2 = null;
-    let c1 = 0, c2 = 0;
-    let len = nums.length;
-    for (let i = 0, len = nums.length; i < len; ++i) {
-        if (nums[i] === m1) {
-            c1 ++;
-        } else if (nums[i] === m2) {
-            c2 ++;
-        } else if (c1 === 0) {
-            m1 = nums[i]; c1 = 1;
-        } else if (c2 === 0) {
-            m2 = nums[i]; c2 = 1;
-        } else {
-            c1 --; c2 --;
-        }
-        //console.log(nums[i]+": m1 =", m1, ", c1 =", c1, "; m2 =", m2, ", c2 =", c2);
-    }
-    // verify
-    c1 = 0, c2 = 0;
-    for (let i = 0; i < len; ++i) {
-        if (nums[i] === m1) c1++;
-        else if (nums[i] === m2) c2++;
-    }
-    let res = [];
-    if (c1 > len / 3) res.push(m1);
-    if (c2 > len / 3) res.push(m2);
-    return res;
+  }
+  // Counting
+  [c1, c2] = [0, 0];
+  for (let n of nums) {
+    if (n === m1) c1++;
+    else if (n === m2) c2++;
+  }
+  let ans = [];
+  if (c1 > nums.length / 3) ans.push(m1);
+  if (c2 > nums.length / 3) ans.push(m2);
+  return ans;
 };
 // TEST
-[/* 
-    [],
-    [1],
-    [1,2],
-    [1,2,3],
-    [1,2,2],
-    [1,1,2,3],
-    [1,1,2,2],*/
-    [1,2,2,3,2,1,1,3]
-].forEach(function (test) {
-    console.log("Majority elements of", test, "->", majorityElement(test));
+[
+  [[], []],
+  [[1], [1]],
+  [
+    [1, 2],
+    [1, 2]
+  ],
+  [[1, 2, 3], []],
+  [[1, 2, 2], [2]],
+  [[1, 1, 2, 3], [1]],
+  [
+    [1, 1, 2, 2],
+    [1, 2]
+  ],
+  [
+    [1, 2, 2, 3, 2, 1, 1, 3],
+    [1, 2]
+  ]
+].forEach(t => {
+  const actual = majorityElement(t[0]);
+  console.log('Majority elements of', t[0], '->', actual);
+  console.assert(actual.length == t[1].length);
+  for (let i = 0; i < actual.length; ++i) {
+    console.assert(actual[i] === t[1][i]);
+  }
 });

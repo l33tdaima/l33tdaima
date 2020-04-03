45b6c5230ff3c47c8ac02e2dc5e77becfa6ed1cc
l33tdaima <jwmiany@gmail.com>
2020-04-03 00:19:20 -0400

    April 30-day challenge, revisited happy number using Python

---
+++ p202e/happy_number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        visited = set()
+        while n != 1:
+            if n in visited:
+                return False
+            visited.add(n)
+            nextn = 0
+            while n != 0:
+                r = n % 10
+                nextn += r * r
+                n = n // 10
+            n = nextn
+        return True
+
+
+# TESTS
+tests = [(19, True), (1, True), (3, False)]
+for t in tests:
+    sol = Solution()
+    actual = sol.isHappy(t[0])
+    print("Is", t[0], "a happy number? ->", actual)

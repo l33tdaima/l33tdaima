6bee47e8d131de65a3c9cd908d06e91db69d2083
l33tdaima <jwmiany@gmail.com>
2020-04-11 14:12:08 -0400

    April 30-day challenge, revisited p543e diameter of binary tree

--- p543e/tree_diameter.js
+++ p543e/tree_diameter.js
@@ -10,33 +10,25 @@ const Tree = require('binary_tree');
  * @param {TreeNode} root
  * @return {number}
  */
-var diameterOfBinaryTree = function(root) {
-    var recDia = function(p) {
-        if (p == null) {
-            return { depth: 0,
-                     dia:   0 };
-        }
-        if (p.left === null && p.right === null) {
-            return { depth: 1,
-                     dia:   0 };
-        }
-        let lt = recDia(p.left);
-        let rt = recDia(p.right);
-        return { depth: Math.max(lt.depth, rt.depth) + 1,
-                 dia: Math.max(lt.dia, rt.dia, lt.depth + rt.depth)};
-    };
-    return recDia(root).dia;
+var diameterOfBinaryTree = function (root) {
+  const helper = (node) => {
+    if (node == null) return [0, 0];
+    const [ldia, ldep] = helper(node.left);
+    const [rdia, rdep] = helper(node.right);
+    return [Math.max(ldia, rdia, ldep + rdep), Math.max(ldep, rdep) + 1];
+  };
+  const [dia] = helper(root);
+  return dia;
 };
 // TEST
 [
-    "#",
-    "1,#,#",
-    "1,2,#,#,#",
-    "1,2,#,#,3,#,#",
-    "1,2,4,#,#,5,#,#,3,#,#",
-    "1,2,4,6,7,#,#,#,#,5,#,8,#,9,#,#,3,#,#"
-].forEach(function(test){
-    let tree = Tree.deserialize(test);
-    console.log("Diagmeter of", test,
-                "->", diameterOfBinaryTree(tree));
+  '#',
+  '1,#,#',
+  '1,2,#,#,#',
+  '1,2,#,#,3,#,#',
+  '1,2,4,#,#,5,#,#,3,#,#',
+  '1,2,4,6,7,#,#,#,#,5,#,8,#,9,#,#,3,#,#',
+].forEach(function (test) {
+  let tree = Tree.deserialize(test);
+  console.log('Diagmeter of', test, '->', diameterOfBinaryTree(tree));
 });

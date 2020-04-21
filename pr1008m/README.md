a8a3c2a869692381766cf0bc02103ff2f85b5a44
l33tdaima <jwmiany@gmail.com>
2020-04-21 00:45:54 -0400

    30-Day LeetCoding Challenge, solved pr1008 BST from preorder in JS, optimal solution helped

---
+++ pr1008m/README.md
@@ -0,0 +1,29 @@
+# 1008. Construct Binary Search Tree from Preorder Traversal (Medium)
+
+Return the root node of a binary search tree that matches the given preorder traversal.
+
+(Recall that a binary search tree is a binary tree where for every node, any descendant of node.left has a value < node.val, and any descendant of node.right has a value > node.val.  Also recall that a preorder traversal displays the value of the node first, then traverses node.left, then traverses node.right.)
+
+### Example 1:
+```
+Input: [8,5,1,7,10,12]
+Output: [8,5,10,1,7,null,12]
+            8
+          /   \
+         5    10
+       /   \    \
+      1    7    12
+```
+
+### Note:
+1. 1 <= preorder.length <= 100
+2. The values of preorder are distinct.
+
+## Solution
+For each root, in order to find out the position in the preorder list after root which splits the left and right sub tree, we can do a simple linear search, which ends up O(N^2) complexity. Or we use binary search to improve to O(NlogN).
+
+Can we do O(N)? We can give the function a bound the maximum number it will handle.
+- The left recursion will take the elements smaller than node.val
+- The right recursion will take the remaining elements smaller than bound
+
+#Tree
---
+++ pr1008m/bst_from_preorder.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @return {TreeNode}
+ */
+const Tree = require('binary_tree');
+var bstFromPreorderON2 = function (preorder) {
+  if (preorder.length === 0) return null;
+  let root = new Tree.TreeNode(preorder[0]);
+  let sp = 1;
+  while (sp < preorder.length && preorder[sp] < preorder[0]) sp++;
+  root.left = bstFromPreorder(preorder.slice(1, sp));
+  root.right = bstFromPreorder(preorder.slice(sp));
+  return root;
+};
+
+var bstFromPreorderON = function (preorder) {
+  let i = 0;
+  const recHelper = (bound = Number.MAX_SAFE_INTEGER) => {
+    if (i === preorder.length || preorder[i] > bound) return null;
+    let root = new Tree.TreeNode(preorder[i++]);
+    root.left = recHelper(root.val);
+    root.right = recHelper(bound);
+    return root;
+  };
+  return recHelper(preorder);
+};
+
+[
+  [[], '#'],
+  [[1], '1,#,#'],
+  [[2, 1], '2,1,#,#,#'],
+  [[2, 3], '2,#,3,#,#'],
+  [[2, 1, 3], '2,1,#,#,3,#,#'],
+  [[8, 5, 1, 7, 10, 12], '8,5,1,#,#,7,#,#,10,#,12,#,#'],
+].forEach((t) => {
+  const actual = bstFromPreorderON(t[0]);
+  console.log(
+    'BST from preorder traversal',
+    t[0],
+    '->',
+    Tree.serialize(actual)
+  );
+  console.assert(Tree.serialize(actual) === t[1]);
+});

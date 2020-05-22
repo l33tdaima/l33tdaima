5b2d9f3573b4111252c82e9f9bcb374a8e39bdaf
l33tdaima <jwmiany@gmail.com>
2020-05-22 00:17:35 -0400

    In place and out of place solutions

--- pr1277m/count_squares.py
+++ pr1277m/count_squares.py
@@ -2,7 +2,21 @@ from typing import List
 
 
 class Solution:
-    def countSquares(self, matrix: List[List[int]]) -> int:
+    # Without modifying input
+    def countSquaresV1(self, matrix: List[List[int]]) -> int:
+        ans, rows, cols = 0, len(matrix), len(matrix[0])
+        dp = [[0 for j in range(cols + 1)] for k in range(rows + 1)]
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == 1:
+                    dp[i + 1][j + 1] = (
+                        min(dp[i + 1][j], dp[i][j + 1], dp[i][j],) + 1
+                    )
+                ans += dp[i + 1][j + 1]
+        return ans
+
+    # Modifying input to achieve O(1) in space
+    def countSquaresV2(self, matrix: List[List[int]]) -> int:
         ans, rows, cols = 0, len(matrix), len(matrix[0])
         for i in range(rows):
             for j in range(cols):
@@ -32,5 +46,8 @@ tests = [
 ]
 for t in tests:
     sol = Solution()
-    actual = sol.countSquares(t[0])
-    print("Square submatrices have all ones ->", actual)
+    actual1 = sol.countSquaresV1(t[0])
+    actual2 = sol.countSquaresV2(t[0])
+    print("Square submatrices have all ones ->", actual2)
+    assert actual1 == t[1]
+    assert actual2 == t[1]

dc41977908d4dc7d015d3cf079540705ad2f6e5e
l33tdaima <jwmiany@gmail.com>
2020-06-21 23:38:09 -0400

    June Leetcoding Challenge: solved p174h dungeon game in Python

---
+++ p174h/calc_min_hp.py
@@ -0,0 +1,24 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        M, N = len(dungeon), len(dungeon[0])
+        hp = defaultdict(lambda: float("Inf"))
+        hp[M, N - 1], hp[M - 1, N] = 1, 1
+        for i in range(M - 1, -1, -1):
+            for j in range(N - 1, -1, -1):
+                hp[i, j] = max(
+                    min(hp[i, j + 1], hp[i + 1, j]) - dungeon[i][j], 1
+                )
+        return hp[0, 0]
+
+
+# TESTS
+tests = [([[-2, -3, 3], [-5, -10, 1], [10, 30, -5]], 7)]
+for t in tests:
+    sol = Solution()
+    actual = sol.calculateMinimumHP(t[0])
+    print("Minimum initial health", t[0], "->", actual)
+

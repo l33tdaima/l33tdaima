a114073ba85bbc558887615a7b1918dbd789fb60
l33tdaima <jwmiany@gmail.com>
2020-06-22 08:24:28 -0400

    June Leetcoding Challenge: Revisited p137m single number II

--- p137m/README.md
+++ p137m/README.md
@@ -5,8 +5,27 @@ Given an array of integers, every element appears three times except for one, wh
 Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 
+### Example 1:
+```
+Input: [2,2,3,2]
+Output: 3
+```
+
+### Example 2:
+```
+Input: [0,1,0,1,0,1,99]
+Output: 99
+```
+
 ## Solution
-Not like p136 where we only need one bit to record the states, we now need two bits to record 3 states (up to 4). The true value table for state transition is like,
+Not like p136 where we only need one bit to record the states, we now need two bits to record 3 states (up to 4).
+
+- First time number appear -> save it in "lsb", 
+- Second time -> clear "lsb" but save it in "msb"
+- Third time -> try to save in "ones" but value saved in "twos" clear it.
+
+The true value table for state transition is like,
+```
 current   incoming  next
 M L            C    M L
 0 0            0    0 0
@@ -18,5 +37,6 @@ M L            C    M L
 
 M = (M ^ C) & ~L
 L = (L ^ C) & ~M
+```
 
 #Bit Manipulation
--- p137m/single_number2.js
+++ p137m/single_number2.js
@@ -2,19 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNumber = function(nums) {
-    let msb = 0, lsb = 0;
-    for (let i = 0, len  = nums.length; i < len; ++i) {
-        lsb = (lsb ^ nums[i]) & ~msb;
-        msb = (msb ^ nums[i]) & ~lsb;
-    }
-    // return state (0,1) since we look for the one appear only once
-    // (1,0) if we look for the one appear twice
-    return lsb;
+var singleNumber = function (nums) {
+  let [msb, lsb] = [0, 0];
+  for (let n of nums) {
+    lsb = (lsb ^ n) & ~msb;
+    msb = (msb ^ n) & ~lsb;
+  }
+  // return state (0,1) since we look for the one appear only once
+  // (1,0) if we look for the one appear twice
+  return lsb;
 };
 // TEST
 [
-    [45,-3,878,-3,45,9999,-3,45,9999,9999],
-].forEach(function(test) {
-    console.log("Single number ->", singleNumber(test));
+  [[2, 2, 3, 2], 3],
+  [[0, 1, 0, 1, 0, 1, 99], 99],
+  [[45, -3, 878, -3, 45, 9999, -3, 45, 9999, 9999], 878],
+].forEach((t) => {
+  const actual = singleNumber(t[0]);
+  console.log('Single number in', t[0], '->', actual);
+  console.assert(t[1] === actual);
 });

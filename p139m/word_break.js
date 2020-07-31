489e1c5cdfc0b9c1e48b393dfd971fc1a2977f39
Mianyu Wang <mwang57@bloomberg.net>
2020-07-31 01:02:42 -0400

    July Leetcoding Challenge: Revisited p140h work break II

--- p139m/word_break.js
+++ p139m/word_break.js
@@ -3,37 +3,37 @@
  * @param {string[]} wordDict
  * @return {boolean}
  */
-var wordBreak = function(s, wordDict) {
-    // build a wordDict map
-    let wordSet = new Set();
-    for (let w of wordDict) {
-        wordSet.add(w);
+var wordBreak = function (s, wordDict) {
+  // build a wordDict map
+  let wordSet = new Set(wordDict);
+  // DP: truth value of substring up to i are word segmented
+  const slen = s.length;
+  const dp = new Array(slen + 1);
+  dp[0] = true; // empty string is segmented
+  for (let i = 1; i <= slen; ++i) {
+    dp[i] = false;
+    for (let j = 0; j < i; ++j) {
+      if (dp[j] && wordSet.has(s.substring(j, i))) {
+        dp[i] = true;
+        break;
+      }
     }
-    // DP: truth value of substring up to i are word segmented
-    let slen = s.length;
-    let dp = new Array(slen + 1);
-    dp[0] = true; // empty string is segmented 
-    for (let i = 1; i <= slen; ++i) {
-        dp[i] = false;
-        for (let j = 0; j < i; ++j) {
-            if (dp[j] && wordSet.has(s.substring(j, i))) {
-                //console.log("Found dp["+j+"] &&", s.substring(j, i));
-                dp[i] = true;
-                break;
-            }
-        }
-    }
-    return dp[slen];
+  }
+  return dp[slen];
 };
 //TEST
 [
-    {s: "l",
-     wordDict: ["let"]},
-    {s: "leetcode",
-     wordDict: ["leet", "code"]},
-    {s: "gogo",
-     wordDict: ["go", "code"]},
-].forEach(function (test) {
-    console.log("Break", test.s, "by wordDict", test.wordDict,
-                "->", wordBreak(test.s, test.wordDict));
+  { s: 'l', wordDict: ['let'], expected: false },
+  { s: 'gogo', wordDict: ['go', 'code'], expected: true },
+  { s: 'leetcode', wordDict: ['leet', 'code'], expected: true },
+  { s: 'applepenapple', wordDict: ['apple', 'pen'], expected: true },
+  {
+    s: 'catsandog',
+    wordDict: ['cats', 'dog', 'sand', 'and', 'cat'],
+    expected: false,
+  },
+].forEach((t) => {
+  const actual = wordBreak(t.s, t.wordDict);
+  console.log('Break', t.s, 'by wordDict', t.wordDict, '->', actual);
+  console.assert(actual === t.expected);
 });

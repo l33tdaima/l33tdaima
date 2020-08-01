b3333d4650b5e48e1e73083bf7b9e6ba635bf62c
Mianyu Wang <mwang57@bloomberg.net>
2020-07-31 22:23:24 -0400

    July Leetcoding Challenge: Revisited p070e climbing stairs

--- p070e/README.md
+++ p070e/README.md
@@ -4,9 +4,6 @@ You are climbing a stair case. It takes n steps to reach to the top.
 
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 
-Note: Given n will be a positive integer.
-
-
 ### Example 1:
 ```
 Input: 2
@@ -26,6 +23,9 @@ Explanation:  There are three ways to climb to the top.
 2. 1 step + 2 steps
 3. 2 steps + 1 step
 ```
+
+### Constraints:
+- 1 <= n <= 45
 
 ## Solution
 ```
--- p070e/climbing_stairs.js
+++ p070e/climbing_stairs.js
@@ -2,7 +2,7 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n) {
+var climbStairs = function (n) {
   // dp[i] is # of ways to climb i steps
   let dp = Array.from({ length: n + 1 }, (v, k) => k); // such that [1,2,...]
   for (let i = 3; i <= n; ++i) {
@@ -10,7 +10,7 @@ var climbStairs = function(n) {
   }
   return dp[n];
 };
-var climbStairsFab = function(n) {
+var climbStairsFab = function (n) {
   let [a, b] = [1, 1];
   while (n--) {
     let t = a;
@@ -20,13 +20,13 @@ var climbStairsFab = function(n) {
   return b;
 };
 // TEST
-for (let n = 1; n <= 20; ++n) {
+for (let n = 1; n <= 45; ++n) {
   console.log(
-    "There are",
+    'There are',
     climbStairsFab(n),
-    "ways to climb to the top of",
+    'ways to climb to the top of',
     n,
-    "steps"
+    'steps'
   );
   console.assert(climbStairs(n) === climbStairsFab(n));
 }

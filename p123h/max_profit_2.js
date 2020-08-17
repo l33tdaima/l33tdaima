330f7b83c137823fc80bff2f574bc5adee0b6676
l33tdaima <l33tdaima@github.com>
2020-08-17 00:04:49 -0400

    August Leetcoding Challenge: Revisited p123h Best Time to Buy and Sell Stock III

--- p123h/max_profit_2.js
+++ p123h/max_profit_2.js
@@ -2,24 +2,27 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-    // dpClose0 = 0, dpOpen1 = -Inf
-    let dpClosed1 = 0, dpClosed2 = 0; // one and two maximum transactions
-    let dpOpen1 = Number.MIN_SAFE_INTEGER, dpOpen2 = Number.MIN_SAFE_INTEGER;
-    for (let p of prices) {
-        dpClosed2 = Math.max(dpClosed2, dpOpen2 + p);
-        dpOpen2   = Math.max(dpOpen2, dpClosed1 - p);
-        dpClosed1 = Math.max(dpClosed1, dpOpen1 + p);
-        dpOpen1   = Math.max(dpOpen1, -p);
-    }
-    return dpClosed2;
+var maxProfit = function (prices) {
+  // dpClose0 = 0, dpOpen0 = -Inf
+  let [dpClosed1, dpClosed2] = [0, 0]; // one and two maximum transactions
+  let [dpOpen1, dpOpen2] = [Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER];
+  for (let p of prices) {
+    dpClosed2 = Math.max(dpClosed2, dpOpen2 + p);
+    dpOpen2 = Math.max(dpOpen2, dpClosed1 - p);
+    dpClosed1 = Math.max(dpClosed1, dpOpen1 + p);
+    dpOpen1 = Math.max(dpOpen1, -p);
+  }
+  return dpClosed2;
 };
 // TEST
 [
-    [1],
-    [7, 1, 5, 3, 6, 4],
-    [7, 6, 4, 3, 1],
-].forEach(t => {
-    console.log("Max profit by 2 transactions in", t, "->",
-                maxProfit(t));
+  [[1], 0],
+  [[7, 1, 5, 3, 6, 4], 7],
+  [[3, 3, 5, 0, 0, 3, 1, 4], 6],
+  [[1, 2, 3, 4, 5], 4],
+  [[7, 6, 4, 3, 1], 0],
+].forEach((t) => {
+  const actual = maxProfit(t[0]);
+  console.log('Max profit by 2 transactions in', t[0], '->', actual);
+  console.assert(actual === t[1]);
 });

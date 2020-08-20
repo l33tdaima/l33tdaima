3d69c1c9f979d68eefafc7ef4168216da6261df8
l33tdaima <l33tdaima@github.com>
2020-08-19 23:12:09 -0400

    August Leetcoding Challenge: Revisited p824e goat latin in JS

--- p824e/goat_latin.js
+++ p824e/goat_latin.js
@@ -2,35 +2,29 @@
  * @param {string} S
  * @return {string}
  */
-var toGoatLatin = function(S) {
-    let isVowel = function (c) {
-        return c === 'a' || c === 'e' || c === 'i' || c === 'o' || c === 'u' ||
-               c === 'A' || c === 'E' || c === 'I' || c === 'O' || c === 'U';
-    };
-    let ans = "";
-    let postfix = "a";
-    let wordList = S.split(' ');
-    for (let [i, word] of wordList.entries()) {
-        if (word.length === 0) { continue; }
-        let glword = "";
-        if (isVowel(word[0])) {
-            glword += word + "ma";
-        } else {
-            glword += word.substr(1) + word[0] + "ma";
-        }
-        glword += postfix;
-        postfix += "a";
-        ans += glword;
-        ans += (i === wordList.length - 1) ? "":" ";
-    }
-    return ans;
+var toGoatLatin = function (S) {
+  const vowels = new Set('aeiouAEIOU');
+  const words = S.split(' ');
+  for (let i = 0; i < words.length; ++i) {
+    if (!vowels.has(words[i][0]))
+      words[i] = words[i].substring(1) + words[i][0];
+    words[i] += 'ma';
+    for (let j = 0; j < i + 1; ++j) words[i] += 'a';
+  }
+  return words.join(' ');
 };
 // TEST
 [
-    "",
-    "a",
-    "I speak Goat Latin",
-    "The quick brown fox jumped over the lazy dog",
-].forEach(t => {
-    console.log(t, "->", toGoatLatin(t));
+  ['a', 'amaa'],
+  ['b', 'bmaa'],
+  ['bc', 'cbmaa'],
+  ['I speak Goat Latin', 'Imaa peaksmaaa oatGmaaaa atinLmaaaaa'],
+  [
+    'The quick brown fox jumped over the lazy dog',
+    'heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa',
+  ],
+].forEach((t) => {
+  const actual = toGoatLatin(t[0]);
+  console.log('Goat Latin of', t[0], '->', actual);
+  console.assert(actual === t[1]);
 });

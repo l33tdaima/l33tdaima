0a3d6b16b12f0a20f436cc0923b5a6a61967d4c0
l33tdaima <l33tdaima@github.com>
2020-09-01 22:37:18 -0400

    September LeetCoding Challenge: Revisited p949e largest time for given digits

--- p949e/largest_time_from_digits.py
+++ p949e/largest_time_from_digits.py
@@ -1,18 +1,24 @@
 from typing import List
-import itertools
+from itertools import permutations
+
 
 class Solution:
     def largestTimeFromDigits(self, A: List[int]) -> str:
-        return max(["%d%d:%d%d" % t for t in itertools.permutations(A) if t[:2] < (2, 4) and t[2] < 6] or [""])
+        return max(
+            ["%d%d:%d%d" % a for a in permutations(A) if a[:2] < (2, 4) and a[2] < 6]
+            or [""]
+        )
+
 
 tests = [
-    ([1,2,3,4], "23:41"),
-    ([5,1,0,1], "15:10"),
-    ([5,5,5,5], "")
+    ([1, 2, 3, 4], "23:41"),
+    ([5, 1, 0, 1], "15:10"),
+    ([5, 5, 5, 5], ""),
+    ([5, 2, 1, 4], "21:54"),
 ]
 
-for input, expected in tests:
+for t, expected in tests:
     sol = Solution()
-    actual = sol.largestTimeFromDigits(input)
-    print("Largest time from digits", input, "->", actual)
-    
+    actual = sol.largestTimeFromDigits(t)
+    print("Largest time from digits", t, "->", actual)
+    assert actual == expected

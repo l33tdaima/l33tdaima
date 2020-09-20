8982a67e6e8f89f10a00ed40659eb00c635d68b4
l33tdaima <l33tdaima@github.com>
2020-09-19 22:23:00 -0400

    September Leetcoding Challenge: Solved p1291m sequential digits in Python

---
+++ pr1291m/sequential_digits.py
@@ -0,0 +1,56 @@
+from typing import List
+
+
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        candidates = [
+            12,
+            23,
+            34,
+            45,
+            56,
+            67,
+            78,
+            89,
+            123,
+            234,
+            345,
+            456,
+            567,
+            678,
+            789,
+            1234,
+            2345,
+            3456,
+            4567,
+            5678,
+            6789,
+            12345,
+            23456,
+            34567,
+            45678,
+            56789,
+            123456,
+            234567,
+            345678,
+            456789,
+            1234567,
+            2345678,
+            3456789,
+            12345678,
+            23456789,
+            123456789,
+        ]
+        return [n for n in candidates if n >= low and n <= high]
+
+
+# TESTS
+tests = [
+    (100, 300, [123, 234]),
+    (1000, 13000, [1234, 2345, 3456, 4567, 5678, 6789, 12345]),
+]
+for low, high, expected in tests:
+    sol = Solution()
+    actual = sol.sequentialDigits(low, high)
+    print("Sequential digits in [", low, ",", high, "] ->", actual)
+    assert actual == expected

f3cce1414668a97765771d732a1a9653a7260d61
l33tdaima <l33tdaima@github.com>
2020-09-24 22:40:16 -0400

    September Leetcoding Challenge: Solved p389e Find the difference in Python

---
+++ p389e/find_difference.py
@@ -0,0 +1,28 @@
+from collections import Counter
+
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        diff = Counter(t) - Counter(s)
+        return list(diff.keys())[0]
+
+    def findTheDifferenceXor(self, s: str, t: str) -> str:
+        code = 0
+        for ch in s + t:
+            code ^= ord(ch)
+        return chr(code)
+
+
+# TESTS
+tests = [
+    ("abcd", "abcde", "e"),
+    ("abcd", "abcdd", "d"),
+    ("aaaaa", "aaaaaa", "a"),
+    ("aaaaa", "aabaaa", "b"),
+]
+for s, t, expected in tests:
+    sol = Solution()
+    actual = sol.findTheDifference(s, t)
+    print("The letter that was added in", t, "from", s, "->", actual)
+    assert actual == expected
+    assert expected == sol.findTheDifferenceXor(s, t)

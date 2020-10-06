1662af2d58bbcadd81be9635b5196a70116b37b5
l33tdaima <l33tdaima@github.com>
2020-10-05 22:58:53 -0400

    October Leetcoding Challenge: Revisited pr1009e complement of base 10 integer

--- pr1009e/bitwise_complement.py
+++ pr1009e/bitwise_complement.py
@@ -6,11 +6,12 @@ class Solution:
         return i ^ N  # i - N
 
     def bitwiseComplementV2(self, N: int) -> int:
-        return N ^ ((1 << N.bit_length()) - 1)
+        return N ^ ((1 << N.bit_length()) - 1) if N else 1
 
 
 # TESTS
 tests = [
+    [0, 1],
     [5, 2],
     [1, 0],
     [7, 0],
@@ -20,7 +21,8 @@ tests = [
     [999999, 48576],
     [12345678, 4431537],
 ]
-for t in tests:
+for N, expected in tests:
     sol = Solution()
-    actual = sol.bitwiseComplement(t[0])
-    assert actual == t[1] == sol.bitwiseComplementV2(t[0])
+    actual = sol.bitwiseComplement(N)
+    assert actual == expected
+    assert sol.bitwiseComplementV2(N) == expected

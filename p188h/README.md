c84facce5ca774bdc318716cec13a9968cb2584d
l33tdaima <l33tdaima@github.com>
2020-10-18 22:21:04 -0400

    October Leetcoding Challenge: Revisited p188h Best Time to Buy and Sell Stock IV in JS

--- p188h/README.md
+++ p188h/README.md
@@ -1,13 +1,37 @@
 # 188. Best Time to Buy and Sell Stock IV (Hard)
 
-Say you have an array for which the ith element is the price of a given stock on day i.
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
 
 Design an algorithm to find the maximum profit. You may complete at most k transactions.
 
 ### Note:
+
 You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
 
+### Example 1:
+
+```
+Input: k = 2, prices = [2,4,1]
+Output: 2
+Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+```
+
+### Example 2:
+
+```
+Input: k = 2, prices = [3,2,6,5,0,3]
+Output: 7
+Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4. Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+```
+
+### Constraints:
+
+- 0 <= k <= 10^9
+- 0 <= prices.length <= 10^4
+- 0 <= prices[i] <= 1000
+
 ## Solution
+
 An excellent generalization of the whole series can be found in this [post](https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/discuss/108870/Most-consistent-ways-of-dealing-with-the-series-of-stock-problems)
 
 In general, stock problem can be characterized by three factors, the ordinal of the day i, the maximum number of allowable transactions k and the number of stocks in our hand at the end of the day.
@@ -18,7 +42,9 @@ This is the most general case, if k is greater than half of the length and more 
 dpClosed[i][k] = max(dpClosed[i-1][k], dpOpen[i-1][k] + prices[i])
 dpOpen[i][k]   = max(dpOpen[i-1][k], dpClosed[i-1][k-1] - prices[i])
 ```
-with base case, 
+
+with base case,
+
 ```
 dpClosed[0][k] = 0, dpClosed[i][0] = 0; // no stock owned
 dpOpen[0][k] = -Inf, dpOpen[i][0] = -Inf; // impossible to have stock at beginning without buying first
--- p188h/max_profit_k.js
+++ p188h/max_profit_k.js
@@ -3,38 +3,46 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(k, prices) {
-    if (k === 0) { return 0; }
-    let len = prices.length;
-    if (k >= ~~(len/2)) {
-        // equivalent to Inf transactions
-        let ansInf = 0;
-        for (let i = 1; i < len; ++i) {
-            if (prices[i] > prices[i-1]) {
-                ansInf += prices[i] - prices[i-1];
-            }
-        }
-        return ansInf;
+var maxProfit = function (k, prices) {
+  if (k === 0) return 0;
+  if (k >= ~~(prices.length / 2)) {
+    // equivalent to Inf transactions
+    let ansInf = 0;
+    for (let i = 1; i < prices.length; ++i) {
+      if (prices[i] > prices[i - 1]) {
+        ansInf += prices[i] - prices[i - 1];
+      }
     }
-    let dpClosed = Array.from({length: k+1}, v => 0);
-    let dpOpen   = Array.from({length: k+1}, v => Number.MIN_SAFE_INTEGER);
-    for (let p of prices) {
-        for (let j = k; j > 0; --j) {
-            dpClosed[j] = Math.max(dpClosed[j], dpOpen[j] + p);
-            dpOpen[j]   = Math.max(dpOpen[j],   dpClosed[j-1] - p);
-        }
+    return ansInf;
+  }
+  let dpClosed = Array.from({ length: k + 1 }, (v) => 0);
+  let dpOpen = Array.from({ length: k + 1 }, (v) => Number.MIN_SAFE_INTEGER);
+  for (let p of prices) {
+    for (let j = k; j > 0; --j) {
+      dpClosed[j] = Math.max(dpClosed[j], dpOpen[j] + p);
+      dpOpen[j] = Math.max(dpOpen[j], dpClosed[j - 1] - p);
     }
-    return dpClosed[k];
+  }
+  return dpClosed[k];
 };
 // TEST
 [
-    [[2], 0],
-    [[1,3], 2],
-    [[1,3,2,8,4,9], 2],
-    [[7,1,5,3,6,4], 5],
-    [[7,6,4,3,1], 3],
-].forEach(t => {
-    console.log("Max profit you can make from",
-                t[0], "with k =", t[1], "->",
-                maxProfit(t[1], t[0]));
+  [[2], 0, 0],
+  [[1, 3], 2, 2],
+  [[2, 4, 1], 2, 2],
+  [[3, 2, 6, 5, 0, 3], 2, 7],
+  [[1, 3, 2, 8, 4, 9], 2, 12],
+  [[7, 1, 5, 3, 6, 4], 5, 7],
+  [[7, 6, 4, 3, 1], 3, 0],
+].forEach(([prices, k, expected]) => {
+  const actual = maxProfit(k, prices);
+  console.log(
+    'Max profit you can make from',
+    prices,
+    'with k =',
+    k,
+    '->',
+    actual
+  );
+  console.assert(actual === expected);
 });

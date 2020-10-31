65797c3a7620d1ab4ed543f4394861f39700a4bf
l33tdaima <l33tdaima@github.com>
2020-10-31 00:19:22 -0400

    October Leetcoding Challenge: Revisited p673m Number of Longest Increasing Subsequence in JS

--- p673m/README.md
+++ p673m/README.md
@@ -1,45 +1,56 @@
 # 673. Number of Longest Increasing Subsequence (Medium)
 
-Given an unsorted array of integers, find the number of longest increasing subsequence.
+Given an integer array nums, return the number of longest increasing subsequences.
 
 ### Example 1:
+
+```
 Input: [1,3,5,4,7]
-
 Output: 2
-
 Explanation: The two longest increasing subsequence are [1, 3, 4, 7] and [1, 3, 5, 7].
+```
 
 ### Example 2:
+
+```
 Input: [2,2,2,2,2]
-
 Output: 5
-
 Explanation: The length of longest continuous increasing subsequence is 1, and there are 5 subsequences' length is 1, so output 5.
+```
 
-Note: Length of the given array will be not exceed 2000 and the answer is guaranteed to be fit in 32-bit signed int.
+### Constraints:
+
+- 0 <= nums.length <= 2000
+- -10^6 <= nums[i] <= 10^6
 
 ## Solution
 
 ### Brutal Force Approach
+
 Over all the combinations of elements of array, each element could contribute the longest increasing subsequence, like 0-1 Knapsack problem, we need to search 2^N space.
 
 ### Dynamic Programming O(N^2)
+
 We need dynamic programming transition for length of LIS used in [#300m](../p300m/README.md) first,
+
 ```
     LIS'(A[0..n-1]) := length of LIS ends with A[n-1]
     LIS'(A[0..n-1]) = max(LIS'(A[0..i])) + 1, for all 0 <= i < n-1, s.t. A[n-1] > A[i]
     LIS(A[0..n-1]) = max(LIS'(A[0..i])), 0 <= i <= n-1
 ```
+
 and also dynamic programming transition for number of LIS,
+
 ```
     NLIS'(A[0..n-1]) := number of LIS ends with A[n-1]
     NLIS'(A[0..n-1]) = sum(NLIS'(A[0..i])), for all 0 <= i < n-1, s.t. A[n-1] > A[i] and LIS'(A[0..i]) == LIS'(A[0..n-1]) - 1
     NLIS(A[0..n-1]) = sum(NLIS'(A[0..i])), for all 0 <= i <= n-1, s.t. LIS'(A[0..i]) == LIS(A[0..n-1])
 ```
+
 ![Example](number_of_lis.png)
 
 #FB
 
 #Array #Dynamic Programming
 
-#Similar question [#300m](../p300m/README.md) [#673m](../p673m/README.md) [#674e](../p674e/README.md) 
+#Similar question [#300m](../p300m/README.md) [#673m](../p673m/README.md) [#674e](../p674e/README.md)
--- p673m/number_of_lis.js
+++ p673m/number_of_lis.js
@@ -2,45 +2,44 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findNumberOfLIS = function(nums) {
-    let len = nums.length;
-    if (len === 0) { return 0; }
-    let dpLIS = Array.from({length: len}, v => 1);
-    let dpNLIS = Array.from({length: len}, v => 1);
-    let maxlen = 1;
-    for (let i = 1; i < len; ++i) {
-        for (let j = 0; j < i; ++j) {
-            if (nums[i] <= nums[j]) { continue; }
-            // This is an optimized version based on the knowledge
-            // that in qualified nums[j], LIS is sympototically
-            // increasing, if we are not 100% sure about this, we
-            // should safely do two loops to compute dpLIS first,
-            // dpNLIS second
-            if (dpLIS[j] + 1 > dpLIS[i]) {
-                dpLIS[i] = dpLIS[j] + 1;
-                dpNLIS[i] = dpNLIS[j];
-            } else if (dpLIS[j] + 1 === dpLIS[i]) {
-                dpNLIS[i] += dpNLIS[j];
-            }
-        }
-        maxlen = Math.max(maxlen, dpLIS[i]);
+var findNumberOfLIS = function (nums) {
+  let N = nums.length;
+  if (N <= 1) return N;
+  let lis = Array.from({ length: N }, (v) => 1);
+  let nlis = Array.from({ length: N }, (v) => 1);
+  let maxlen = 1;
+  for (let i = 1; i < N; ++i) {
+    for (let j = 0; j < i; ++j) {
+      if (nums[i] <= nums[j]) continue;
+      // This is an optimized version based on the knowledge
+      // that in qualified nums[j], LIS is sympototically
+      // increasing, if we are not 100% sure about this, we
+      // should safely do two loops to compute LIS first,
+      // NLIS second
+      if (lis[j] >= lis[i]) {
+        lis[i] = lis[j] + 1;
+        nlis[i] = nlis[j];
+      } else if (lis[j] + 1 === lis[i]) {
+        nlis[i] += nlis[j];
+      }
     }
-    let ans = 0;
-    for (let i = 0; i < len; ++i) {
-        if (dpLIS[i] === maxlen) {
-            ans += dpNLIS[i];
-        }
-    }
-    return ans;
+    maxlen = Math.max(maxlen, lis[i]);
+  }
+  let ans = 0;
+  for (let i = 0; i < N; ++i) {
+    if (lis[i] === maxlen) ans += nlis[i];
+  }
+  return ans;
 };
 // TEST
 [
-    [],
-    [1],
-    [1,2,4,2,3],
-    [1,3,5,4,7],
-    [2,2,2,2,2],
-].forEach(t => {
-    console.log("Number of LIS of", t, "->",
-                findNumberOfLIS(t));
+  [[], 0],
+  [[1], 1],
+  [[1, 2, 4, 2, 3], 3],
+  [[1, 3, 5, 4, 7], 2],
+  [[2, 2, 2, 2, 2], 5],
+].forEach(([nums, expected]) => {
+  const actual = findNumberOfLIS(nums);
+  console.log('Number of LIS of', nums, '->', actual);
+  console.assert(actual === expected);
 });

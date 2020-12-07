09ac78278a32f141e4b32364ad22bec31104e0ba
l33tdaima <l33tdaima@github.com>
2020-12-06 22:46:29 -0500

    December Leetcoding Challenge: Revisited p117m Populating Next Right Pointers in Each Node II in JS

--- p117m/README.md
+++ p117m/README.md
@@ -1,40 +1,49 @@
 # 117. Populating Next Right Pointers in Each Node II (Medium)
 
 Given a binary tree
+
 ```
-struct TreeLinkNode {
-    TreeLinkNode *left;
-    TreeLinkNode *right;
-    TreeLinkNode *next;
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
 }
 ```
+
 Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
 
 Initially, all next pointers are set to NULL.
 
-### Note:
+### Follow up:
+
 - You may only use constant extra space.
 - Recursive approach is fine, implicit stack space does not count as extra space for this problem.
 
-### Example
-Given the following binary tree,
+### Example 1:
+
 ```
      1
    /  \
   2    3
  / \    \
 4   5    7
-```
-After calling your function, the tree should look like:
-```
+
      1 -> NULL
    /  \
   2 -> 3 -> NULL
  / \    \
 4-> 5 -> 7 -> NULL
+
+Input: root = [1,2,3,4,5,null,7]
+Output: [1,#,2,3,#,4,5,7,#]
+Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
 ```
+
 ## Solution
+
 The intuitive way is to do BFS and set up the links, but restricted constant extra space, we do similar to [#116m](../p116m/README.md)
+
 - Move level by level down by moving the linked list head, we need to figure out the head for the next level when iterating this level
 - When iterating the linked link in each level, which is linked during the upper level iteration,
   - If left exists: Is there a prev node? connect prev.next = p.left : set the head of next level to p.left;
--- p117m/connect_next_right2.js
+++ p117m/connect_next_right2.js
@@ -1,57 +1,53 @@
 /**
- * Definition for binary tree with next pointer.
- * function TreeLinkNode(val) {
- *     this.val = val;
- *     this.left = this.right = this.next = null;
- * }
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
  */
 const Tree = require('binary_tree_link');
 /**
- * @param {TreeLinkNode} root
- * @return {void} Do not return anything, modify tree in-place instead.
+ * @param {Node} root
+ * @return {Node}
  */
-var connect = function(root) {
-    if (root == null) { return; }
-    let pHead = root;
-    let nHead = null; // head of next level, to be set when iterating this level
-    let nPrev = null; // holder of prev node when traversing next level
-    while (pHead !== null) {
-        let p = pHead;
-        while (p !== null) { // iterate horizon within this level
-            if (p.left !== null) {
-                if (nPrev === null) {
-                    nHead = p.left;
-                } else {
-                    nPrev.next = p.left;
-                }
-                nPrev = p.left;
-            }
-            if (p.right !== null) {
-                if (nPrev === null) {
-                    nHead = p.right;
-                } else {
-                    nPrev.next = p.right;
-                }
-                nPrev = p.right;
-            }
-            p = p.next;
-        }
-        pHead = nHead;
-        nHead = null;
-        nPrev = null;
+var connect = function (root) {
+  if (root == null) return root;
+  // nHead: haead of next level, to be set when iterating this level pHead
+  // nPrev: holder of prev node when traversing next level
+  let [pHead, nHead, nPrev] = [root, null, null];
+  while (pHead) {
+    let p = pHead;
+    while (p) {
+      // iterate horizon within this level
+      if (p.left) {
+        if (nPrev) nPrev.next = p.left;
+        else nHead = p.left;
+        nPrev = p.left;
+      }
+      if (p.right) {
+        if (nPrev) nPrev.next = p.right;
+        else nHead = p.right;
+        nPrev = p.right;
+      }
+      p = p.next;
     }
+    [pHead, nHead, nPrev] = [nHead, null, null];
+  }
+  return root;
 };
 // TEST
 [
-    "#",
-    "1,#,#",
-    "1,2,4,#,#,5,#,#,3,#,7,#,#",
-    "1,2,4,#,#,#,3,6,#,#,#",
-    "1,2,#,4,#,#,3,#,7,#,#",
-].forEach(t => {
-    let tree = Tree.deserializeNoLink(t);
-    connect(tree);
-    console.log("Dump links of tree ->");
-    console.log(" ", t);
-    console.log(" ", Tree.serializeLink(tree));
+  '#',
+  '1,#,#',
+  '1,2,4,#,#,5,#,#,3,#,7,#,#',
+  '1,2,4,#,#,#,3,6,#,#,#',
+  '1,2,#,4,#,#,3,#,7,#,#',
+].forEach((t) => {
+  let tree = Tree.deserializeNoLink(t);
+  connect(tree);
+  console.log('Dump links of tree ->');
+  console.log(' ', t);
+  console.log(' ', Tree.serializeLink(tree));
 });

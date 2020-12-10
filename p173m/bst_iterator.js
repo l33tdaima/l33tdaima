9e35a1f02857656ccb6b46131476daa7d52f9c57
l33tdaima <l33tdaima@github.com>
2020-12-09 23:37:29 -0500

    December Leetcoding Challenge: Revisited p173m Binary Search Tree Iterator in J

--- p173m/bst_iterator.js
+++ p173m/bst_iterator.js
@@ -10,47 +10,48 @@ const Tree = require('binary_tree');
  * @constructor
  * @param {TreeNode} root - root of the binary search tree
  */
-var BSTIterator = function(root) {
-    this.pushLeft = function (p, stack) {
-        while (p !== null) {
-            stack.push(p);
-            p = p.left;
-        }
-    };
-    this.stack = new Array();
-    this.pushLeft(root, this.stack);
+var BSTIterator = function (root) {
+  this.pushLeft = function (p, stack) {
+    while (p) {
+      stack.push(p);
+      p = p.left;
+    }
+  };
+  this.stack = new Array();
+  this.pushLeft(root, this.stack);
 };
 /**
  * @this BSTIterator
  * @returns {boolean} - whether we have a next smallest number
  */
-BSTIterator.prototype.hasNext = function() {
-    return this.stack.length > 0;
+BSTIterator.prototype.hasNext = function () {
+  return this.stack.length > 0;
 };
 /**
  * @this BSTIterator
  * @returns {number} - the next smallest number
  */
-BSTIterator.prototype.next = function() {
-    let top = this.stack.pop();
-    this.pushLeft(top.right, this.stack);
-    return top.val;
+BSTIterator.prototype.next = function () {
+  let top = this.stack.pop();
+  this.pushLeft(top.right, this.stack);
+  return top.val;
 };
 /**
  * Your BSTIterator will be called like this:
  * var i = new BSTIterator(root), a = [];
  * while (i.hasNext()) a.push(i.next());
-*/
+ */
 // TEST
 [
-    "",
-    "1",
-    "2,1,#,#,3,#,#",
-    "5,3,2,1,#,#,#,4,#,#,7,6,#,#,8,#,9,#,#",
-].forEach(t => {
-    let tree = Tree.deserialize(t);
-    let i = new BSTIterator(tree);
-    let a = [];
-    while (i.hasNext()) a.push(i.next());
-    console.log("BSTIterator for", t, "->", a);
+  ['', []],
+  ['1', [1]],
+  ['2,1,#,#,3,#,#', [1, 2, 3]],
+  ['7,3,#,#,15,9,#,#,20,#,#', [3, 7, 9, 15, 20]],
+  ['5,3,2,1,#,#,#,4,#,#,7,6,#,#,8,#,9,#,#', [1, 2, 3, 4, 5, 6, 7, 8, 9]],
+].forEach(([t, expected]) => {
+  const i = new BSTIterator(Tree.deserialize(t));
+  const actual = [];
+  while (i.hasNext()) actual.push(i.next());
+  console.log('Dump BSTIterator for', t, '->', actual);
+  console.assert(actual.toString() === expected.toString());
 });

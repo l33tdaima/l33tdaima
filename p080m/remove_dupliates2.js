dba3513c639b865dded047f2a19c10b6ff925ad5
l33tdaima <l33tdaima@github.com>
2020-12-12 00:11:52 -0500

    December Leetcoding Challenge: Revisited p080m Remove Duplicates from Sorted Array II in Python

--- p080m/remove_dupliates2.js
+++ p080m/remove_dupliates2.js
@@ -2,24 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
-    let k = 0;
-    for (let i = 0, len = nums.length; i < len; ++i) {
-        if (k < 2 || (nums[i] > nums[k - 2])) {
-            nums[k++] = nums[i];
-        }
-    }
-    return k;
+var removeDuplicates = function (nums) {
+  let k = 0;
+  for (let n of nums) {
+    if (k < 2 || n > nums[k - 2]) nums[k++] = n;
+  }
+  return k;
 };
 // TEST
 [
-    [[1,1,1,2,2,3], 5],
-    [[0,0,1,1,1,1,2,3,3], 7],
-    [[3,3,3,4,5,6,6,7,8,9], 9]
-].forEach(t => {
-    console.log("Original array", t[0]);
-    let reducedLength = removeDuplicates(t[0]);
-    console.log("After removing duplicates ->",
-                reducedLength, t[0].slice(0, reducedLength), "\n");
-    console.assert(t[1] === reducedLength);
+  [[], 0],
+  [[1], 1],
+  [[2, 2], 2],
+  [[1, 1, 1, 2, 2, 3], 5],
+  [[0, 0, 1, 1, 1, 1, 2, 3, 3], 7],
+  [[3, 3, 3, 4, 5, 6, 6, 7, 8, 9], 9],
+].forEach(([nums, expected]) => {
+  const copy = Array.from(nums);
+  let actual = removeDuplicates(nums);
+  console.log('Removing duplicates from', nums, '->', nums.slice(0, actual));
+  console.assert(actual === expected);
 });
--- p080m/remove_dupliates2.py
+++ p080m/remove_dupliates2.py
@@ -5,7 +5,7 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         k = 0
         for n in nums:
-            if k < 1 or n != nums[k - 1]:
+            if k < 2 or n != nums[k - 2]:
                 nums[k] = n
                 k += 1
         return k
@@ -13,14 +13,15 @@ class Solution:
 
 # TESTS
 for nums, expected in [
-    [[], 0],
-    [[1], 1],
-    [[1, 1, 1, 2, 2, 3], 3],
-    [[0, 0, 1, 1, 1, 1, 2, 3, 3], 4],
-    [[3, 3, 3, 4, 5, 6, 6, 7, 8, 9], 7],
+    ([], 0),
+    ([1], 1),
+    ([2, 2], 2),
+    ([1, 1, 1, 2, 2, 3], 5),
+    ([0, 0, 1, 1, 1, 1, 2, 3, 3], 7),
+    ([3, 3, 3, 4, 5, 6, 6, 7, 8, 9], 9),
 ]:
     sol = Solution()
     copy = list(nums)
     actual = sol.removeDuplicates(nums)
-    print("Remove duplicates from", copy, "->", nums[:actual])
+    print("Remove duplidates from", copy, "->", nums[:actual])
     assert actual == expected

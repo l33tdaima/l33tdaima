abdea16d10d2d2682a49e28da3c49bae7b849b04
l33tdaima <l33tdaima@github.com>
2020-12-15 01:08:40 -0500

    December Leetcoding Challenge: Revisited p131m Palindrome Partition in JS

--- p131m/README.md
+++ p131m/README.md
@@ -1,20 +1,32 @@
 # 131. Palindrome Partition (Medium)
 
-Given a string s, partition s such that every substring of the partition is a palindrome.
+Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
 
-Return all possible palindrome partitioning of s.
+A palindrome string is a string that reads the same backward as forward.
 
-For example, given s = "aab",
-Return
+### Example 1:
 
-[
-  ["aa","b"],
-  ["a","a","b"]
-]
+```
+Input: s = "aab"
+Output: [["a","a","b"],["aa","b"]]
+```
+
+### Example 2:
+
+```
+Input: s = "a"
+Output: [["a"]]
+```
+
+### Constraints:
+
+- 1 <= s.length <= 16
+- s contains only lowercase English letters.
 
 ## Solution:
 
 Challenges are covering the cases reaching the end of string.
+
 1. No palindrome found for this segment, e.g. "ab", returns [] and the outside recursion should NOT count this no solution case;
 2. Exactly one palindrome found this segment, return [s] and stop any more recursion.
 
--- p131m/palindrome_partition.js
+++ p131m/palindrome_partition.js
@@ -2,45 +2,48 @@
  * @param {string} s
  * @return {string[][]}
  */
-var partition = function(s) {
-    // Palindrome check
-    var isPalindrome = function(s, start, end) {
-        for (let i = start, j = end; i < j; ++i, --j) {
-            if (s.charAt(i) !== s.charAt(j)) {
-                return false;
-            }
-        }
-        return true;
-    };
+var partition = function (s) {
+  // Palindrome check
+  const isPalindrome = function (s) {
+    for (let l = 0, r = s.length - 1; l < r; ++l, --r) {
+      if (s[l] !== s[r]) return false;
+    }
+    return true;
+  };
 
-    // Recursion to DFS partial solution
-    var recPartitionHelper = function(s, start, end) {
-        // console.log("Current stack:", s.substring(start, end + 1));
-        let solution = [];
-        for (let i = start; i <= end; ++i) {
-            if (isPalindrome(s, start, i)) {
-                let baseSolution = [ s.substring(start, i + 1) ];
-                if (i === end) {
-                    // no more room to backtracking
-                    solution.push(baseSolution);
-                }
-                else {
-                    // solve for partition for the rest of the string
-                    let partSolution = recPartitionHelper(s, i + 1, end);
-                    // console.log("# Partial solution:", partSolution);
-                    for (let j = 0; j < partSolution.length; ++j)
-                    {
-                        solution.push(baseSolution.concat(partSolution[j]));
-                        // console.log("# Solution so far:", solution);
-                    }
-                }
-            }
-        }
-        // console.log("Returning stack solution:", solution);
-        return solution;
-    };
-    return recPartitionHelper(s, 0, s.length - 1);
+  const ans = [];
+  const dfs = function (s, path) {
+    if (!s) {
+      ans.push(Array.from(path));
+      return;
+    }
+
+    for (let i = 1; i <= s.length; ++i) {
+      if (isPalindrome(s.substring(0, i))) {
+        path.push(s.substring(0, i));
+        dfs(s.substring(i), path);
+        path.pop();
+      }
+    }
+    return ans;
+  };
+
+  dfs(s, []);
+  return ans;
 };
 
-var strTest = (process.argv[2] === undefined) ? "" : process.argv[2];
-console.log(partition(strTest));
+// TESTS
+[
+  [
+    'aab',
+    [
+      ['a', 'a', 'b'],
+      ['aa', 'b'],
+    ],
+  ],
+  ['a', [['a']]],
+].forEach(([s, expected]) => {
+  const actual = partition(s);
+  console.log('All possible palindrome partitioning of', s, '->', actual);
+  console.assert(actual.toString() === expected.toString());
+});

fc168f5fe8a19b8f37cd16041ab6a09efb9faf63
l33tdaima <l33tdaima@github.com>
2020-12-31 23:24:20 -0500

    December Leetcoding Challenge: Revisited p084h Largest Rectangle in Histogra in JS

--- p084h/README.md
+++ p084h/README.md
@@ -2,23 +2,29 @@
 
 Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
 
+![pic](histogram_area.png)
+
 Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
 
 The largest rectangle is shown in the shaded area, which has area = 10 unit.
 
 ### Example
-![pic](histogram_area.png)
+
 ```
 Input: [2,1,5,6,2,3],
 return 10.
 ```
 
 ## Solution
+
 http://www.geeksforgeeks.org/largest-rectangle-under-histogram/
+
 ### Intuitive Approach
+
 For each bar, search left and right for the first bar smaller than it, which are the boundary of rectangle area,`area = (r - l - 1) * height[i]`, then we get the max after loop. O(n^2)
 
 ### Optimal Approach
+
 Similar to above, for every bar ‘x’, we calculate the area with ‘x’ as the smallest bar in the rectangle, where we need to identify the left and right boundary of the area. To achieve O(n), the left bound search can be achieved by a stack storing the increasing history, while right bound search is gradually built up by moving the current bar.
 
 We keep pushing the bar into stack when the height is increasing, and start calulate the area when height is decreasing by poping the previous bars. The area is calculated by multiplying this smallest bar by the index difference.
--- p084h/largest_rec_area.js
+++ p084h/largest_rec_area.js
@@ -2,47 +2,45 @@
  * @param {number[]} heights
  * @return {number}
  */
-var largestRectangleArea = function(heights) {
-    let stack = []; // stack to store the indices
-    let maxArea = 0;
-    for (let i = 0, len = heights.length; i < len || stack.length > 0;) {
-        let sz = stack.length;
-        //console.log("\n# Loop", i, "Stack", stack);
-        // i out of bound will yield undefined and hence false for the check below
-        if (sz === 0 || heights[i] >= heights[stack[sz - 1]]) {
-        // push the increasing histogram bar index into the stack
-            //console.log("+ Push [" + i + "], height", heights[i]);
-            stack.push(i++);
-        } else {
-        // pop the decreasing histogram bar index from the stack
-            let itop = stack.pop();
-            //console.log("- Pop [" + itop + "], height", heights[itop]);
-            let remained = stack.length;
-            // Case 1: when poping the last item which is the global minimum,
-            // we should calcuate the area by the full length, e.g. [2,1,2]
-            // Case 2: if not last item, the width of area should be from remained index + 1 to i
-            // eg. [1,4,3] -> 3*(3-1-0) = 6
-            let area = heights[itop] * (remained === 0 ? i : (i - (1 + stack[remained - 1])));
-            if (area > maxArea) { maxArea = area; }
-            //console.log("Max area so far", maxArea);
-        }
+var largestRectangleArea = function (heights) {
+  let [stack, ans] = [[], 0]; // stack to store the indices
+  for (let i = 0; i < heights.length || stack.length > 0; ) {
+    let sz = stack.length;
+    // i out of bound will yield undefined and hence false for the check below
+    if (sz === 0 || heights[i] >= heights[stack[sz - 1]]) {
+      // push the increasing histogram bar index into the stack
+      stack.push(i++);
+    } else {
+      // pop the decreasing histogram bar index from the stack
+      let itop = stack.pop();
+      let remained = stack.length;
+      // Case 1: when poping the last item which is the global minimum,
+      // we should calcuate the area by the full length, e.g. [2,1,2]
+      // Case 2: if not last item, the width of area should be from remained index + 1 to i
+      // eg. [1,4,3] -> 3*(3-1-0) = 6
+      ans = Math.max(
+        ans,
+        heights[itop] * (remained === 0 ? i : i - (stack[remained - 1] + 1))
+      );
+      //console.log("Max area so far", maxArea);
     }
-    return maxArea;
+  }
+  return ans;
 };
 // TEST
 [
-    [[2], 2],
-    [[2,5], 5],
-    [[4,3], 6],
-    [[1,4,3], 6],
-    [[2,1,2], 3],
-    [[1,5,4,3], 9],
-    [[2,0,3,2], 4],
-    [[2,1,5,6,2,3], 10],
-    [[2,1,5,6,2,3,6,7], 12],
-    [[6,7,5,2,4,5,9,3], 16]
-].forEach(function (test) {
-    let res = largestRectangleArea(test[0]);
-    console.log("Largest Rectangle Area ->", res,
-                "?", res === test[1]);
+  [[2], 2],
+  [[2, 5], 5],
+  [[4, 3], 6],
+  [[1, 4, 3], 6],
+  [[2, 1, 2], 3],
+  [[1, 5, 4, 3], 9],
+  [[2, 0, 3, 2], 4],
+  [[2, 1, 5, 6, 2, 3], 10],
+  [[2, 1, 5, 6, 2, 3, 6, 7], 12],
+  [[6, 7, 5, 2, 4, 5, 9, 3], 16],
+].forEach(([heights, expected]) => {
+  const actual = largestRectangleArea(heights);
+  console.log('Largest Rectangle Area in', heights, '->', actual);
+  console.assert(actual === expected);
 });

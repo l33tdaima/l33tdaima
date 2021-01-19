ad6fb1c942e6675710e5d85545f90f27dce24e17
l33tdaima <l33tdaima@github.com>
2021-01-18 20:07:39 -0500

    January 2021 Leetcoding Challenge: Solved pr1679m Max Number of K-Sum Pairs in Python

---
+++ pr1679m/max_operations.py
@@ -0,0 +1,25 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        seen, ans = defaultdict(int), 0
+        for n in nums:
+            if k - n in seen and seen[k - n] > 0:
+                ans += 1
+                seen[k - n] -= 1
+            else:
+                seen[n] += 1
+        return ans
+
+
+# TESTS
+for nums, k, expected in [
+    ([1, 2, 3, 4], 5, 2),
+    ([3, 1, 3, 4, 3], 6, 1),
+]:
+    sol = Solution()
+    actual = sol.maxOperations(nums, k)
+    print(f"The max number of {k}-sum pairs in {nums} ->", actual)
+    assert actual == expected

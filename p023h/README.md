0a1085d7a55b23921f673d0f656be202787eb705
l33tdaima <l33tdaima@github.com>
2021-01-25 00:24:10 -0500

    January 2021 Leetcoding Challenge: Revisited p023h Merge k Sorted Lists in JS

--- p023h/README.md
+++ p023h/README.md
@@ -1,8 +1,51 @@
 #23. Merge k Sorted Lists (Hard)
 
-Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
 
-#### Solution:
+Merge all the linked-lists into one sorted linked-list and return it.
+
+### Example 1:
+
+```
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+```
+
+### Example 2:
+
+```
+Input: lists = []
+Output: []
+```
+
+### Example 3:
+
+```
+Input: lists = [[]]
+Output: []
+```
+
+### Constraints:
+
+- k == lists.length
+- 0 <= k <= 10^4
+- 0 <= lists[i].length <= 500
+- -10^4 <= lists[i][j] <= 10^4
+- lists[i] is sorted in ascending order.
+- The sum of lists[i].length won't exceed 10^4.
+
+## Solution:
+
 1. build-in sort
 2. merge sort
 3. priority queue: Use a (min) heap to store the head of each linked list, and extract one at a time, once move the node to the sorted result, push the next to the heap keep them sorted.
+
+#Linked List #Divide and Conquer #Heap
--- p023h/merge_k_lists.js
+++ p023h/merge_k_lists.js
@@ -10,54 +10,48 @@ const List = require('list');
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-var merge2Lists = function(lst1, lst2) {
-    if (!lst1) {
-        return lst2;
+var merge2Lists = function (lst1, lst2) {
+  if (!lst1) return lst2;
+  if (!lst2) return lst1;
+  const hd = new List.ListNode(0);
+  let p = hd;
+  while (lst1 && lst2) {
+    if (lst1.val < lst2.val) {
+      p.next = lst1;
+      lst1 = lst1.next;
+    } else {
+      p.next = lst2;
+      lst2 = lst2.next;
     }
-    if (!lst2) {
-        return lst1;
-    }
-    var hd = new List.ListNode(0);
-    var p = hd;
-    while (lst1 && lst2) {
-        if(lst1.val < lst2.val) {
-            p.next = lst1;
-            lst1 = lst1.next;
-        }
-        else {
-            p.next = lst2;
-            lst2 = lst2.next;
-        }
-        p = p.next;
-    }
-    p.next = (lst1)?lst1:lst2;
-    return hd.next;
+    p = p.next;
+  }
+  p.next = lst1 ? lst1 : lst2;
+  return hd.next;
 };
 
-var mergeKLists = function(lists) {
-    if (lists.length === 0) {
-        return lists;
-    }
-    if (lists.length === 1) {
-        return lists[0];
-    }
-    let mid = ~~(lists.length/2); // integer quotient
-    // console.log("k:", lists.length, ", mid:", mid);
-    let left = mergeKLists(lists.slice(0, mid));
-    let right = mergeKLists(lists.slice(mid));
-    return merge2Lists(left, right);
+var mergeKLists = function (lists) {
+  if (lists.length === 0) return null;
+  if (lists.length === 1) return lists[0];
+  let mid = ~~(lists.length / 2);
+  let left = mergeKLists(lists.slice(0, mid));
+  let right = mergeKLists(lists.slice(mid));
+  return merge2Lists(left, right);
 };
 
-var testArray = [
-    [1, 3, 5],
-    [2, 4, 6],
-    [0, 6, 9, 10],
-    [8, 8, 8]
-];
-
-var testLists = [];
-testArray.forEach(function(elem) {
-    testLists.push(List.fromArray(elem));
-}, this);
-var merged = mergeKLists(testLists);
-console.log("Merged K Sorted Lists:", List.toArray(merged));
+[
+  [
+    [
+      [1, 4, 5],
+      [1, 3, 4],
+      [2, 6],
+    ],
+    [1, 1, 2, 3, 4, 4, 5, 6],
+  ],
+  [[], []],
+  [[[]], []],
+].forEach(([tests, expected]) => {
+  const lists = tests.map(List.fromArray);
+  const actual = List.toArray(mergeKLists(lists));
+  console.log('Merged K Sorted Lists', tests, '->', actual);
+  console.assert(actual.toString() === expected.toString());
+});

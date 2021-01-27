0b4c5d41b7437c603b9e2bf27075c8eb28d85b04
l33tdaima <l33tdaima@github.com>
2021-01-27 00:29:41 -0500

    January 2021 Leetcoding Challenge: Solved pr1631m Path With Minimum Effort in Python with help

---
+++ pr1631m/minimum_effort_path.py
@@ -0,0 +1,44 @@
+from typing import List
+from heapq import heappop, heappush
+
+
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        rows, cols = len(heights), len(heights[0])
+        dist = [[float("inf")] * cols for _ in range(rows)]
+        minheap = [(0, 0, 0)]  # (dist, row, col)
+        DIR = [0, 1, 0, -1, 0]
+        while minheap:
+            d, r, c = heappop(minheap)
+            if d > dist[r][c]:
+                continue
+            if r == rows - 1 and c == cols - 1:
+                return d
+            for i in range(4):
+                nr, nc = r + DIR[i], c + DIR[i + 1]
+                if 0 <= nr < rows and 0 <= nc < cols:
+                    newdist = max(d, abs(heights[nr][nc] - heights[r][c]))
+                    if newdist < dist[nr][nc]:
+                        dist[nr][nc] = newdist
+                        heappush(minheap, (newdist, nr, nc))
+
+
+# TESTS
+for heights, expected in [
+    ([[1, 2, 2], [3, 8, 2], [5, 3, 5]], 2),
+    ([[1, 2, 3], [3, 8, 4], [5, 3, 5]], 1),
+    (
+        [
+            [1, 2, 1, 1, 1],
+            [1, 2, 1, 2, 1],
+            [1, 2, 1, 2, 1],
+            [1, 2, 1, 2, 1],
+            [1, 1, 1, 2, 1],
+        ],
+        0,
+    ),
+]:
+    sol = Solution()
+    actual = sol.minimumEffortPath(heights)
+    print("Minimum effort travelling", heights, "->", actual)
+    assert actual == expected

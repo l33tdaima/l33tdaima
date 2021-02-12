a60f81e89668c0d499597c2650071c38aa65e32a
l33tdaima <l33tdaima@github.com>
2021-02-11 23:09:22 -0500

    February 2021 Leetcoding Challenge: Revisited p242e Valid Anagram in JS

--- p242e/README.md
+++ p242e/README.md
@@ -2,15 +2,28 @@
 
 Given two strings s and t, write a function to determine if t is an anagram of s.
 
-For example,
-s = "anagram", t = "nagaram", return true.
-s = "rat", t = "car", return false.
+### Example 1:
 
-Note:
+```
+Input: s = "anagram", t = "nagaram"
+Output: true
+```
+
+### Example 2:
+
+```
+Input: s = "rat", t = "car"
+Output: false
+```
+
+### Note:
+
 You may assume the string contains only lowercase alphabets.
 
-Follow up:
+### Follow up:
+
 What if the inputs contain unicode characters? How would you adapt your solution to such case?
 
 #AMZN #UBER #YELP
+
 #Hash Table #Sort
--- p242e/valid_anagram.js
+++ p242e/valid_anagram.js
@@ -3,43 +3,36 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagramUnicode = function(s, t) {
-    let sArray = s.split("");
-    sArray.sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0));
-    let tArray = t.split("");
-    tArray.sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0));
-    return sArray.join("") === tArray.join("");
+var isAnagramUnicode = function (s, t) {
+  let sArray = s.split('');
+  sArray.sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0));
+  let tArray = t.split('');
+  tArray.sort((a, b) => a.charCodeAt(0) - b.charCodeAt(0));
+  return sArray.join('') === tArray.join('');
 };
-var isAnagram = function(s, t) {
-    let slen = s.length;
-    if (slen !== t.length) { return false; }
-    let signature = Array.from({length: 26}, (v) => 0);
-    for (let i = 0; i < slen; ++i) {
-        signature[s.charCodeAt(i) - 97] ++;
-        signature[t.charCodeAt(i) - 97] --;
-    }
-    for (let i = 0; i < 26; ++i) {
-        if (signature[i] !== 0) { return false; }
-    }
-    return true;
+
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+  let signature = Array.from({ length: 26 }, (v) => 0);
+  for (let i = 0; i < s.length; ++i) {
+    signature[s.charCodeAt(i) - 97]++;
+    signature[t.charCodeAt(i) - 97]--;
+  }
+  for (let i = 0; i < 26; ++i) {
+    if (signature[i] !== 0) return false;
+  }
+  return true;
 };
 
 // TEST
 [
-    ["rat", "cat"],
-    ["eat", "tea"],
-    ["anagram", "nagaram"],
-    ["成功", "功成"],
-    ["成功", "功夫"],
-].forEach(function (test) {
-    console.log("Are", test[0], "and", test[1], "unicode anagram? ->",
-                isAnagramUnicode(test[0], test[1]));
-});
-[
-    ["rat", "cat"],
-    ["eat", "tea"],
-    ["anagram", "nagaram"],
-].forEach(function (test) {
-    console.log("Are", test[0], "and", test[1], "ascii anagram? ->",
-                isAnagram(test[0], test[1]));
+  ['rat', 'cat', false],
+  ['eat', 'tea', true],
+  ['anagram', 'nagaram', true],
+  ['成功', '功成', true],
+  ['成功', '功夫', false],
+].forEach(([s, t, expected]) => {
+  const actual = isAnagramUnicode(s, t);
+  console.log('Are', s, 'and', t, 'unicode anagram? ->', actual);
+  console.assert(actual === expected);
 });

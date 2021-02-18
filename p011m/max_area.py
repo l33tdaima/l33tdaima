ccc90424669a910bd08a4e90e5d6066d3b83ce94
l33tdaima <l33tdaima@github.com>
2021-02-17 23:29:35 -0500

    February 2021 Leetcoding Challenge: Solved p011m Container With Most Water in Python

---
+++ p011m/max_area.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        ans = 0
+        while l < r:
+            ans = max(ans, (r - l) * min(height[l], height[r]))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return ans
+
+
+# TESTS
+for height, expected in [
+    ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
+    ([1, 1], 1),
+    ([4, 3, 2, 1, 4], 16),
+    ([1, 2, 1], 2),
+]:
+    sol = Solution()
+    actual = sol.maxArea(height)
+    print("The most water in", height, "->", actual)
+    assert actual == expected

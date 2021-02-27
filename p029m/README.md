7db42a8136dc1a4e376775d6a213dc2b383aa907
l33tdaima <l33tdaima@github.com>
2021-02-27 17:38:21 -0500

    February 2021 Leetcoding Challenge: Revisited p029m Divide Two Integers in C++

--- p029m/README.md
+++ p029m/README.md
@@ -4,24 +4,49 @@ Given two integers dividend and divisor, divide two integers without using multi
 
 Return the quotient after dividing dividend by divisor.
 
-The integer division should truncate toward zero.
+The integer division should truncate toward zero, which means losing its fractional part. For example, truncate(8.345) = 8 and truncate(-2.7335) = -2.
+
+### Note:
+
+Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−2^31, 2^31 − 1]. For this problem, assume that your function returns 2^31 − 1 when the division result overflows.
 
 ### Example 1:
+
 ```
 Input: dividend = 10, divisor = 3
 Output: 3
+Explanation: 10/3 = truncate(3.33333..) = 3.
 ```
+
 ### Example 2:
+
 ```
 Input: dividend = 7, divisor = -3
 Output: -2
+Explanation: 7/-3 = truncate(-2.33333..) = -2.
 ```
-### Note:
-- Both dividend and divisor will be 32-bit signed integers.
-- The divisor will never be 0.
-- Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−2^31,  2^31 − 1]. For the purpose of this problem, assume that your function returns 2^31 − 1 when the division result overflows.
+
+### Example 3:
+
+```
+Input: dividend = 0, divisor = 1
+Output: 0
+```
+
+### Example 4:
+
+```
+Input: dividend = 1, divisor = 1
+Output: 1
+```
+
+### Constraints:
+
+- -2^31 <= dividend, divisor <= 2^31 - 1
+- divisor != 0
 
 ## Solution
+
 Essentially we will keep substracting divisor from dividend, which is suboptimal if divisor is significantly smaller than dividend, will cause Time Limit Exceeded violation.
 
 We will need to accelarate this substraction by left shift (multiply by 2) divisor each iteation.
--- p029m/divide.cpp
+++ p029m/divide.cpp
@@ -1,5 +1,6 @@
 // g++ -std=c++11 *.cpp -o test && ./test && rm test
 
+#include <cassert>
 #include <iostream>
 #include <vector>
 
@@ -7,37 +8,38 @@ using namespace std;
 
 class Solution {
 public:
-    int divide(int dividend, int divisor) {
-        if (dividend == INT_MIN && divisor == -1) {
+    int divide(int dividend, int divisor)
+    {
+        if (dividend == INT_MIN && divisor == -1)
             return INT_MAX;
-        }
-        bool negative = (dividend < 0) ^ (divisor < 0);
-        long n = abs(dividend), d = abs(divisor), ans = 0;
+        // abs(INT_MIN) is undefined behavior according http://www.cplusplus.com/reference/cstdlib/abs/
+        long n = abs(static_cast<long>(dividend)), d = abs(static_cast<long>(divisor));
+        int ans = 0, x = 0;
         while (n >= d) {
-            long md = d, mul = 1;
-            while (n >= (md << 1)) {
-                md <<= 1;
-                mul <<= 1;
-            }
-            n -= md;
-            ans += mul; 
+            for (x = 0; n - (d << x << 1) >= 0; x++) { }
+            n -= d << x;
+            ans += 1 << x;
         }
-        return negative ? -ans : ans;
+        return (dividend > 0) ^ (divisor > 0) ? -ans : ans;
     }
 };
 // TEST
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
     vector<vector<int>> tests = {
-        {10, 3},
-        {7, -3},
-        {INT_MIN, -1},
+        { 10, 3, 3 },
+        { 7, -3, -2 },
+        { 0, 1, 0 },
+        { 1, 1, 1 },
+        { -2147483648, -1, INT_MAX },
+        { -2147483648, 3, -715827882 }, // works on Leetcode C++ compiler, but not on Apple clang 12
     };
     Solution sol;
-    for (auto& t: tests) {
-        cout << t[0] << "/" << t[1] << " = " 
-             << sol.divide(t[0], t[1])
-             << endl;
+    for (auto& t : tests) {
+        int actual = sol.divide(t[0], t[1]);
+        cout << t[0] << "/" << t[1] << " = "
+             << actual << endl;
+        assert(actual == t[2]);
     }
     return 0;
 }

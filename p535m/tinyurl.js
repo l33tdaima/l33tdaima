8d7a7de93426ef38f49a81ff93559e087f362c43
l33tdaima <l33tdaima@github.com>
2021-03-15 23:49:28 -0400

    March 2021 Leetcoding Challenge: Revisited p535m Tiny URL in JS

--- p535m/tinyurl.js
+++ p535m/tinyurl.js
@@ -1,6 +1,7 @@
-const charmap = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const CHARMAP =
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
-var short2long = new Map();
+const short2long = new Map();
 
 /**
  * Encodes a URL to a shortened URL.
@@ -8,20 +9,21 @@ var short2long = new Map();
  * @param {string} longUrl
  * @return {string}
  */
-var encode = function(longUrl) {
-    var genShortUrl = function () {
-        let rand = [];
-        for (let i = 0; i < 6; ++i) {
-            rand.push( charmap[~~(Math.random() * 61)] );
-        }
-        return rand.join('');
+var encode = function (longUrl) {
+  const genShortUrl = function () {
+    let rand = [];
+    for (let i = 0; i < 6; ++i) {
+      rand.push(CHARMAP[~~(Math.random() * 61)]);
     }
-    let shortUrl = null;
-    do {
-        shortUrl = genShortUrl();
-    } while (short2long.get(shortUrl) !== undefined);
-    short2long.set(shortUrl, longUrl);
-    return shortUrl;
+    return rand.join('');
+  };
+
+  let shortUrl = null;
+  do {
+    shortUrl = genShortUrl();
+  } while (short2long.get(shortUrl) !== undefined);
+  short2long.set(shortUrl, longUrl);
+  return shortUrl;
 };
 
 /**
@@ -30,8 +32,8 @@ var encode = function(longUrl) {
  * @param {string} shortUrl
  * @return {string}
  */
-var decode = function(shortUrl) {
-    return short2long.get(shortUrl);
+var decode = function (shortUrl) {
+  return short2long.get(shortUrl);
 };
 
 /**
@@ -39,10 +41,12 @@ var decode = function(shortUrl) {
  * decode(encode(url));
  */
 [
-    "",
-    "a",
-    "https://leetcode.com/problems/design-tinyurl",
-].forEach(function (test) {
-    console.log("TinyURL of", test, "->", encode(test));
-    console.assert(decode(encode(test)) === test);
+  '',
+  'abc',
+  'https://leetcode.com/problems/design-tinyurl',
+  'https://leetcode.com/problems/design-tinyurl',
+].forEach((longUrl) => {
+  const shortUrl = encode(longUrl);
+  console.log('TinyURL of', longUrl, '->', shortUrl);
+  console.assert(decode(shortUrl) === longUrl);
 });

b3d00311eb9c4ac0a8d2afcb99d00df7c7680493
l33tdaima <l33tdaima@github.com>
2021-04-01 23:47:50 -0400

    April 2021 Leetcoding Challenge: Revisited p234e Palindrome Linked List in JS

--- p234e/README.md
+++ p234e/README.md
@@ -1,21 +1,30 @@
 # 234. Palindrome Linked List (Easy/Medium)
-Given a singly linked list, determine if it is a palindrome.
+
+Given the head of a singly linked list, return true if it is a palindrome.
 
 ### Example 1:
+
 ```
-Input: 1->2
-Output: false
-```
-### Example 2:
-```
-Input: 1->2->2->1
+Input: head = [1->2->2->1]
 Output: true
 ```
 
-Follow up:
-Could you do it in O(n) time and O(1) space?
+### Example 2:
+
+```
+Input: head = [1->2]
+Output: false
+```
+
+### Constraints:
+
+- The number of nodes in the list is in the range [1, 10^5].
+- 0 <= Node.val <= 9
+
+_Follow up:_ Could you do it in O(n) time and O(1) space?
 
 ## Solution
+
 - Look for the mid point using fast slow approach.
 - Reverse the linked list when moving slow pointer.
 - From the mid point, move towards both direction to do palindrome check.
--- p234e/palindrome_list.js
+++ p234e/palindrome_list.js
@@ -10,29 +10,32 @@ const List = require('list');
  * @param {ListNode} head
  * @return {boolean}
  */
-var isPalindrome = function(head) {
-    var slow = head;
-    var fast = head;
-    var rev = null;
-    while(fast && fast.next) {
-        fast = fast.next.next;
-        // reverse the slow iteration
-        var temp = rev;
-        rev = slow;
-        slow = slow.next;
-        rev.next = temp;
-    }
-    if(fast) {
-        slow = slow.next;
-    }
-    while(rev && rev.val === slow.val) {
-        slow = slow.next;
-        rev = rev.next;
-    }
-    return !rev;
+var isPalindrome = function (head) {
+  let [slow, fast, rev] = [head, head, null];
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    // reverse the slow iteration
+    let temp = rev;
+    rev = slow;
+    slow = slow.next;
+    rev.next = temp;
+  }
+  if (fast) slow = slow.next;
+  while (rev && rev.val === slow.val) {
+    slow = slow.next;
+    rev = rev.next;
+  }
+  return !rev;
 };
 
-console.log("Test Case []: " + isPalindrome(List.fromArray([])));
-console.log("Test Case [1,2,1]: " + isPalindrome(List.fromArray([1,2,1])));
-console.log("Test Case [1,2,3,4]: " + isPalindrome(List.fromArray([1,2,3,4])));
-console.log("Test Case [1,2,2,1]: " + isPalindrome(List.fromArray([1,2,2,1])));
+// TESTS
+[
+  [[], true],
+  [[1, 2, 2, 1], true],
+  [[1, 2], false],
+  [[1, 2, 1], true],
+].forEach(([array, expected]) => {
+  const actual = isPalindrome(List.fromArray(array));
+  console.log('List', array, 'is palindrome? ->', actual);
+  console.assert(actual === expected);
+});

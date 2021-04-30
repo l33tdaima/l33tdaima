5e047a3b83f7180b7745b22a8d3b5f90ea5ea8d4
l33tdaima <l33tdaima@github.com>
2021-04-29 22:33:23 -0400

    April 2021 Leetcoding Challenge: Revisited p034m Search for a Range in Python

--- p034m/search_range.py
+++ p034m/search_range.py
@@ -2,44 +2,43 @@ from typing import List
 
 
 class Solution:
-    def binarySearch(self, nums: List[int], target: int, starting: bool) -> int:
-        l, r = 0, len(nums) - 1
-        while l <= r:
-            mid = l + (r - l) // 2
-            if nums[mid] < target:
-                l = mid + 1
-            elif nums[mid] > target:
-                r = mid - 1
-            else:  # find target
-                if starting:  # search for starting position
-                    if mid == 0 or nums[mid - 1] != target:
-                        return mid
-                    else:
-                        r = mid - 1
-                else:  # search for ending position
-                    if mid == len(nums) - 1 or nums[mid + 1] != target:
-                        return mid
-                    else:
-                        l = mid + 1
-        return -1
-
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        starting = self.binarySearch(nums, target, True)
-        if starting == -1:
+        def binarySearch(l: int, r: int, first: bool) -> int:
+            nonlocal target
+            while l <= r:
+                mid = l + (r - l) // 2
+                if nums[mid] < target:
+                    l = mid + 1
+                elif nums[mid] > target:
+                    r = mid - 1
+                else:  # == target
+                    if first:  # search for the first position
+                        if mid == 0 or nums[mid - 1] != target:
+                            return mid
+                        else:
+                            r = mid - 1
+                    else:  # search for the last position
+                        if mid == len(nums) - 1 or nums[mid + 1] != target:
+                            return mid
+                        else:
+                            l = mid + 1
+            return -1
+
+        first = binarySearch(0, len(nums) - 1, True)
+        if first == -1:
             return [-1, -1]
-        return [starting, self.binarySearch(nums, target, False)]
+        return [first, binarySearch(first, len(nums) - 1, False)]
 
 
 # TESTS
-tests = [
+for nums, target, expected in [
     ([5, 7, 7, 8, 8, 10], 8, [3, 4]),
     ([5, 7, 7, 8, 8, 10], 6, [-1, -1]),
+    ([], 0, [-1, -1]),
     ([5, 7, 7, 8, 8, 10, 10, 10], 5, [0, 0]),
     ([5, 7, 7, 8, 8, 10, 10, 10], 10, [5, 7]),
-]
-
-for t in tests:
+]:
     sol = Solution()
-    actual = sol.searchRange(t[0], t[1])
-    print("Search", t[1], "in", t[0], "->", actual)
-    assert actual == t[2]
+    actual = sol.searchRange(nums, target)
+    print("Search", target, "in", nums, "->", actual)
+    assert actual == expected

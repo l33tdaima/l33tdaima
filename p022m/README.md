616262f62e2554d236c6c92fe4474d8c1564f00c
l33tdaima <l33tdaima@github.com>
2021-06-16 23:07:17 -0400

    June 2021 Leetcoding Challenge: Revisited p022m Generate Parentheses in JS and C++

--- p022m/README.md
+++ p022m/README.md
@@ -1,22 +1,33 @@
 # 22. Generate Parentheses (Medium)
 
-Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+Given `n` pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 
-For example, given n = 3, a solution set is:
+### Example 1:
 
-[
-  "((()))",
-  "(()())",
-  "(())()",
-  "()(())",
-  "()()()"
-]
+```
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+```
+
+### Example 2:
+
+```
+Input: n = 1
+Output: ["()"]
+```
+
+### Constraints:
+
+- 1 <= n <= 8
 
 ## Solution
+
 ### Plus Approach
+
 The idea here is to only add '(' and ')' that we know will guarantee us a solution (instead of adding too many close). Once we add a '(' we will then discard it and try a ')' which can only close a valid '('. Function arguments `open` and `close` represent how many open and close parenthesis have been matched. Recursion ends when both reach to n.
 
 ### Minus Approach
+
 In this approach, function arguments `open` and `close` represent remaining open parenthesis and the close parenthesis to be added, which initiated by call with `n` and `0`.
 
 #GOOGL #UBER #Zenefits
--- p022m/generate_parenthesis.cpp
+++ p022m/generate_parenthesis.cpp
@@ -1,35 +1,34 @@
 // Build program by
 // g++ -std=c++11 *.cpp -o test && ./test && rm test
-#include <vector>
-#include <string>
 #include <iostream>
 #include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 class Solution {
 public:
-    vector<string> generateParenthesis(int n) {
+    vector<string> generateParenthesis(int n)
+    {
         vector<string> ans;
-        if (n < 0) { return ans; }
-        std::function<void(string,int,int)> recBtHelper = 
-        [&](string ps, int open, int close) -> void {
+        std::function<void(string, int, int)> backtrack =
+            [&](string ps, int open, int close) -> void {
             if (open == 0 && close == 0) {
                 ans.push_back(ps);
                 return;
             }
-            if (open > 0) {
-                recBtHelper(ps + "(", open - 1, close + 1);
-            }
-            if (close > 0) {
-                recBtHelper(ps + ")", open, close - 1);
-            }
+            if (open > 0)
+                backtrack(ps + "(", open - 1, close + 1);
+            if (close > 0)
+                backtrack(ps + ")", open, close - 1);
         };
-        recBtHelper("", n, 0);
+        backtrack("", n, 0);
         return ans;
     }
 };
 // TEST
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
     if (argc <= 1) {
         cout << "Please input a number" << endl;
         return 1;
@@ -39,7 +38,7 @@ int main(int argc, char* argv[]) {
     ss >> n;
     Solution sol;
     vector<string> ans = sol.generateParenthesis(n);
-    for (const string& s: ans) {
+    for (const string& s : ans) {
         cout << s << endl;
     }
     return 0;

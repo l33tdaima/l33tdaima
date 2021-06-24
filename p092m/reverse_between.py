e4007c5274c43962dd84cb176a079bd260f5e4a7
l33tdaima <l33tdaima@github.com>
2021-06-23 23:33:15 -0600

    June 2021 Leetcoding Challenge: Solved p092m Reverse Linked List II in Python

---
+++ p092m/reverse_between.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from local_packages.list import ListNode
+
+
+class Solution:
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+        if not head:
+            return head
+        dummy = ListNode(0, head)
+
+        pre, i = dummy, 0
+        while i < left - 1:
+            i, pre = i + 1, pre.next
+
+        start = pre.next
+        then = start.next
+
+        while i < right - 1:
+            start.next = then.next
+            then.next = pre.next
+            pre.next = then
+            then = start.next
+            i += 1
+
+        return dummy.next
+
+
+# TESTS
+for array, left, right, expected in [
+    ([1, 2, 3, 4, 5], 2, 4, [1, 4, 3, 2, 5]),
+    ([5], 1, 1, [5]),
+]:
+    sol = Solution()
+    actual = ListNode.to_array(
+        sol.reverseBetween(ListNode.from_array(array), left, right)
+    )
+    print("Reverse", array, "between", left, "and", right, "->", actual)
+    assert actual == expected

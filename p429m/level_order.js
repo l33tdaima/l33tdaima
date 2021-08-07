dd881a37aa39488e0c9e6da28f769cfe4d19e6c3
l33tdaima <l33tdaima@github.com>
2021-08-06 23:12:06 -0400

    August 2021 Leetcoding Challenge: Revisited p429m N-ary Tree Level Order Traversal in JS

--- p429m/level_order.js
+++ p429m/level_order.js
@@ -10,15 +10,17 @@ const NaryTree = require('n_ary_tree');
  * @param {Node} root
  * @return {number[][]}
  */
-var levelOrder = function(root) {
+var levelOrder = function (root) {
   let ans = [];
   if (root == null) return ans;
-  const recPreorder = function(node, level) {
+
+  const recPreorder = (node, level) => {
     if (node == null) return;
     if (ans[level]) ans[level].push(node.val);
     else ans.push([node.val]);
     for (let c of node.children) recPreorder(c, level + 1);
   };
+
   recPreorder(root, 0);
   return ans;
 };
@@ -29,8 +31,8 @@ var levelOrder = function(root) {
   '1 1 2 0',
   '1 2 2 0 3 0',
   '1 3 2 0 3 0 4 0',
-  '1 3 3 2 5 0 6 0 2 0 4 0'
-].forEach(t => {
+  '1 3 3 2 5 0 6 0 2 0 4 0',
+].forEach((t) => {
   const tree = NaryTree.deserialize(t);
   console.log('Level order traversal of', t, '->', levelOrder(tree));
 });

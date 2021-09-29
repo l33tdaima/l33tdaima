276f9194c115ac903703a5a77e24694c3cf94c05
l33tdaima <l33tdaima@github.com>
2021-09-28 22:47:13 -0400

    September 2021 Leetcoding Challenge: Revisited p922e Sort Array By Parity II in Python

--- p922e/sort_array_by_parity_ii.py
+++ p922e/sort_array_by_parity_ii.py
@@ -1,24 +1,27 @@
 from typing import List
 
+
 class Solution:
-    def sortArrayByParityII(self, A: List[int]) -> List[int]:
-        i_even, i_odd = 0, 1
-        length = len(A)
-        while i_even < length and i_odd < length:
-            if A[i_even] % 2 == 0:
-                i_even += 2
-            elif A[i_odd] % 2 != 0:
-                i_odd += 2
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        even, odd, length = 0, 1, len(nums)
+        while even < length and odd < length:
+            if nums[even] % 2 == 0:
+                even += 2
+            elif nums[odd] % 2 != 0:
+                odd += 2
             else:
-                A[i_even], A[i_odd] = A[i_odd], A[i_even]
-        return A
+                nums[even], nums[odd] = nums[odd], nums[even]
+        return nums
+
+
 # TESTS
-tests = [
-    [1,2],
-    [4,2,5,7],
-    [3,1,2,4]
-]
-for t in tests:
+for nums, expected in [
+    ([1, 2], [2, 1]),
+    ([4, 2, 5, 7], [4, 5, 2, 7]),
+    ([2, 3], [2, 3]),
+    ([3, 1, 2, 4], [4, 1, 2, 3]),
+]:
     sol = Solution()
-    actual = sol.sortArrayByParityII(t)
-    print("Sort array by parity ->", actual)
+    actual = sol.sortArrayByParityII(nums)
+    print("Sort", nums, "by parity ->", actual)
+    assert actual == expected

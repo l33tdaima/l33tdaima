2652fd0a7145468129f28fc500744d40eb988470
l33tdaima <l33tdaima@github.com>
2021-10-02 00:02:07 -0400

    October 2021 Leetcoding Challenge: Revisited p174h Dungeon Game in Python

--- p174h/calc_min_hp.py
+++ p174h/calc_min_hp.py
@@ -9,16 +9,16 @@ class Solution:
         hp[M, N - 1], hp[M - 1, N] = 1, 1
         for i in range(M - 1, -1, -1):
             for j in range(N - 1, -1, -1):
-                hp[i, j] = max(
-                    min(hp[i, j + 1], hp[i + 1, j]) - dungeon[i][j], 1
-                )
+                hp[i, j] = max(min(hp[i, j + 1], hp[i + 1, j]) - dungeon[i][j], 1)
         return hp[0, 0]
 
 
 # TESTS
-tests = [([[-2, -3, 3], [-5, -10, 1], [10, 30, -5]], 7)]
-for t in tests:
+for dungeon, expected in [
+    ([[-2, -3, 3], [-5, -10, 1], [10, 30, -5]], 7),
+    ([[0]], 1),
+]:
     sol = Solution()
-    actual = sol.calculateMinimumHP(t[0])
-    print("Minimum initial health", t[0], "->", actual)
-
+    actual = sol.calculateMinimumHP(dungeon)
+    print("Minimum initial health", dungeon, "->", actual)
+    assert actual == expected

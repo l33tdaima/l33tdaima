69bf9f5f1b2fe5f220764a102cdd29319ad37f2a
l33tdaima <l33tdaima@github.com>
2021-10-09 22:55:43 -0400

    October 2021 Leetcoding Challenge: Revisited p201m Bitwise AND of Numbers Range in Python

--- p201m/README.md
+++ p201m/README.md
@@ -1,17 +1,30 @@
 # 201. Bitwise AND of Numbers Range (Medium)
 
-Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+Given two integers `left` and `right` that represent the range `[left, right]`, return the bitwise AND of all numbers in this range, inclusive.
 
 ### Example 1:
+
 ```
-Input: [5,7]
+Input: left = 5, right = 7
 Output: 4
 ```
 
 ### Example 2:
+
 ```
-Input: [0,1]
+Input: left = 0, right = 0
 Output: 0
 ```
+
+### Example 3:
+
+```
+Input: left = 1, right = 2147483647
+Output: 0
+```
+
+### Constraints:
+
+- `0 <= left <= right <= 2^31 - 1`
 
 #Bit Manipulation
--- p201m/range_bitwise_and.py
+++ p201m/range_bitwise_and.py
@@ -1,31 +1,20 @@
 class Solution:
-    # This algorithm iterates from MSB
-    def rangeBitwiseAndMSB(self, m: int, n: int) -> int:
-        ans, mask = 0, 0x100000000
-        while mask > 0:
-            mask = mask >> 1
-            # print(bin(mask), (m & mask), (n & mask))
-            if m & mask == mask and n & mask == mask:
-                ans = ans | mask
-            elif (m & mask) ^ (n & mask):
-                break
-        return ans
-
-    # This algorithm iterates from LSB and faster
-    def rangeBitwiseAndLSB(self, m: int, n: int) -> int:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
         i = 0  # how many bit right shifted
-        while m != n:
-            m >>= 1
-            n >>= 1
+        while left != right:
+            left >>= 1
+            right >>= 1
             i += 1
-        return m << i
+        return left << i
 
 
 # TESTS
-tests = [[5, 7, 4], [0, 1, 0], [26, 30, 24]]
-for t in tests:
+for left, right, expected in [
+    (5, 7, 4),
+    (0, 1, 0),
+    (26, 30, 24),
+]:
     sol = Solution()
-    actual = sol.rangeBitwiseAndMSB(t[0], t[1])
-    print("Bitwise AND of all numbers in range", f"[{t[0]}, {t[1]}] ->", actual)
-    assert actual == t[2]
-    assert t[2] == sol.rangeBitwiseAndLSB(t[0], t[1])
+    actual = sol.rangeBitwiseAnd(left, right)
+    print("Bitwise AND of all numbers in range", f"[{left}, {right}] ->", actual)
+    assert actual == expected

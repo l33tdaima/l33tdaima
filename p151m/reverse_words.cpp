28dd2e57e32465b5e9ab56e8d431ec444add3abf
l33tdaima <l33tdaima@github.com>
2021-10-19 22:18:12 -0400

    October 2021 Leetcoding Challenge: Revisited p151m Reverse Words in a String in Python and C++

--- p151m/reverse_words.cpp
+++ p151m/reverse_words.cpp
@@ -1,23 +1,28 @@
 // g++ -std=c++11 *.cpp -o test && ./test && rm -f test
+#include <algorithm>
+#include <cassert>
+#include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 class Solution {
 public:
-    string reverseWords(string s) {
+    string reverseWords(string s)
+    {
         int ri = 0;
         for (int i = 0, l = 0; i < s.size();) {
             // move to the begin of a valid word
-            while (i < s.size() && s[i] == ' ') i++;
-            // set single space of the reduced string 
-            if (i < s.size() && ri > 0) s[ri++] = ' ';
+            while (i < s.size() && s[i] == ' ')
+                i++;
+            // set single space of the reduced string
+            if (i < s.size() && ri > 0)
+                s[ri++] = ' ';
             l = ri;
             // copy to remove multiple spaces
-            while (i < s.size() && s[i] != ' ') s[ri++] = s[i++];
+            while (i < s.size() && s[i] != ' ')
+                s[ri++] = s[i++];
             std::reverse(s.begin() + l, s.begin() + ri);
         }
         s.resize(ri);
@@ -29,7 +34,8 @@ public:
 struct Test {
     string s;
     string expected;
-    void run() {
+    void run()
+    {
         cout << "Reverse '" << s << "' -> '";
         Solution sol;
         string actual = sol.reverseWords(s);
@@ -37,17 +43,17 @@ struct Test {
         assert(actual == expected);
     }
 };
-int main() {
+int main()
+{
     vector<Test> tests = {
-        {"", ""},
-        {"thesky", "thesky"},
-        {" t  e a ", "a e t"},
-        {"the sky is blue", "blue is sky the"},
-        {"  hello world!  ", "world! hello"},
-        {"a good   example", "example good a"}
+        { "", "" },
+        { "thesky", "thesky" },
+        { " t  e a ", "a e t" },
+        { "the sky is blue", "blue is sky the" },
+        { "  hello world!  ", "world! hello" },
+        { "a good   example", "example good a" }
     };
-    for (auto& t: tests) {
+    for (auto& t : tests)
         t.run();
-    }
     return 0;
 }

94e53d3f951161dd78b88e82d0f792a1445f86af
l33tdaima <l33tdaima@github.com>
2021-11-20 16:30:04 -0500

    November 2021 Leetcoding Challenge: Revisited p540m Single Element in a Sorted Array in Python

--- p540m/README.md
+++ p540m/README.md
@@ -2,21 +2,28 @@
 
 You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once. Find this single element that appears only once.
 
+Return the single element that appears only once.
+
+Your solution must run in O(log n) time and O(1) space.
+
 ### Example 1:
+
 ```
 Input: [1,1,2,3,3,4,4,8,8]
 Output: 2
 ```
 
 ### Example 2:
+
 ```
 Input: [3,3,7,7,10,11,11]
 Output: 10
 ```
- 
+
 Note: Your solution should run in O(log n) time and O(1) space.
 
 ## Solution
-The non-sorted version of this problem is [#136](../p136e/README.md) can be solved in O(n). 
+
+The non-sorted version of this problem is [#136](../p136e/README.md) can be solved in O(n).
 
 #Similar questions Find single [#136](../p136e/README.md) Find dup [#287](../p287m/README.md) Find non-dup [#540m](../p540m/README.md)
--- p540m/single_non_dup.py
+++ p540m/single_non_dup.py
@@ -8,30 +8,29 @@ class Solution:
             m = (l + r) // 2
             if nums[m - 1] != nums[m] != nums[m + 1]:
                 return nums[m]
-            elif nums[m - 1] == nums[m]:
-                if (r - m) % 2 == 0:  # even number on m's rhs
-                    r = m - 2
-                else:
+            elif nums[m] != nums[m + 1]:  # split between m and m + 1
+                if (m + 1 - l) % 2 == 0:  # even on left
                     l = m + 1
-            else:  # nums[m] == nums[r]
-                if (m - l) % 2 == 0:  # even number on m's left
-                    l = m + 2
-                else:
+                else:  # odd on left
+                    r = m
+            else:  # split between m - 1 and m
+                if (r + 1 - m) % 2 == 0:  # even on right
                     r = m - 1
+                else:  # odd on right
+                    l = m
         return nums[l]
 
 
 # TESTS
-tests = [
+for nums, expected in [
     ([1, 1, 2], 2),
     ([1, 2, 2, 3, 3], 1),
     ([2, 2, 3, 5, 5], 3),
     ([2, 2, 3, 3, 5], 5),
     ([1, 1, 2, 3, 3, 4, 4, 8, 8], 2),
     ([3, 3, 7, 7, 10, 11, 11], 10),
-]
-for t in tests:
+]:
     sol = Solution()
-    actual = sol.singleNonDuplicate(t[0])
-    print("The single non-duplicate in", t[0], "->", actual)
-    assert actual == t[1]
+    actual = sol.singleNonDuplicate(nums)
+    print("The single non-duplicate in", nums, "->", actual)
+    assert actual == expected

a1951dd51563f63f77807864c3a853257f4f3f8d
l33tdaima <l33tdaima@github.com>
2021-11-27 23:28:53 -0500

    November 2021 Leetcoding Challenge: Revisited p797m All Paths From Source to Target in Python

--- p797m/all_paths_source_target.py
+++ p797m/all_paths_source_target.py
@@ -3,32 +3,33 @@ from typing import List
 
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        ans, visited = [], [False for _ in graph]
+        n, ans = len(graph), []
 
-        def helper(target: int, path: List[int]) -> None:
-            if path[-1] == target:
-                ans.append(list(path))
-                return
-            for v in graph[path[-1]]:
-                path.append(v)
-                visited[v] = True
-                helper(target, path)
-                visited[v] = False
-                path.pop()
+        def dfs(path: List[int]) -> None:
+            if path[-1] == n - 1:
+                ans.append(path[:])
+            else:
+                for v in graph[path[-1]]:
+                    path.append(v)
+                    dfs(path)
+                    path.pop()
 
-        visited[0] = True
-        helper(target=len(graph) - 1, path=[0])
+        dfs([0])
         return ans
 
 
 # TESTS
-tests = [
-    ([[1], []], [[0, 1]]),
-    ([[1], [0]], [[0, 1]]),
+for graph, expected in [
     ([[1, 2], [3], [3], []], [[0, 1, 3], [0, 2, 3]]),
-]
-for t in tests:
+    (
+        [[4, 3, 1], [3, 2, 4], [3], [4], []],
+        [[0, 4], [0, 3, 4], [0, 1, 3, 4], [0, 1, 2, 3, 4], [0, 1, 4]],
+    ),
+    ([[1], []], [[0, 1]]),
+    ([[1, 2, 3], [2], [3], []], [[0, 1, 2, 3], [0, 2, 3], [0, 3]]),
+    ([[1, 3], [2], [3], []], [[0, 1, 2, 3], [0, 3]]),
+]:
     sol = Solution()
-    actual = sol.allPathsSourceTarget(t[0])
-    print("All paths in graph", t[0], "->", actual)
-    assert sorted(actual) == t[1]
+    actual = sol.allPathsSourceTarget(graph)
+    print("All paths in graph", graph, "->", actual)
+    assert sorted(actual) == sorted(expected)

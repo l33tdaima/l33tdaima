8e6ecad69aca1e3bf2189340b33e086fbedc5d8f
l33tdaima <l33tdaima@github.com>
2021-12-01 22:44:30 -0500

    December 2021 Leetcoding Challenge: Revisited p328m Odd Even Linked List in Python

--- p328m/README.md
+++ p328m/README.md
@@ -1,23 +1,41 @@
 # 328. Odd Even Linked List (Medium)
 
-Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+Given the `head` of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
 
-You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+The _first_ node is considered _odd_, and the _second_ node is _even_, and so on.
+
+Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+You must solve the problem in O(1) extra space complexity and O(n) time complexity.
 
 ### Example 1:
+
 ```
-Input: 1->2->3->4->5->NULL
-Output: 1->3->5->2->4->NULL
+1->2->3->4->5->NULL
+      ||
+      \/
+1->3->5->2->4->NULL
+
+Input: head = [1,2,3,4,5]
+Output: [1,3,5,2,4]
 ```
 
 ### Example 2:
+
 ```
-Input: 2->1->3->5->6->4->7->NULL
-Output: 2->3->6->7->1->5->4->NULL
+2->1->3->5->6->4->7->NULL
+        ||
+        \/
+2->3->6->7->1->5->4->NULL
+
+Input: head = [2,1,3,5,6,4,7]
+Output: [2,3,6,7,1,5,4]
 ```
 
-### Note:
-- The relative order inside both the even and odd groups should remain as it was in the input.
-- The first node is considered odd, the second node even and so on ...
+### Constraints:
+
+- `n ==` number of nodes in the linked list
+- `0 <= n <= 10^4`
+- `-10^6 <= Node.val <= 10^6`
 
 #Linked List
--- p328m/odd_even_list.py
+++ p328m/odd_even_list.py
@@ -3,52 +3,34 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from typing import Optional
 from local_packages.list import ListNode
 
 
 class Solution:
-    # By move the odd node in front of even head
-    def oddEvenListV1(self, head: ListNode) -> ListNode:
-        if not head:
-            return None
-        p, pev = head, head.next
-        while pev and pev.next:
-            # insert the odd node after p
-            odd = pev.next
-            pev.next = odd.next
-            odd.next = p.next
-            p.next = odd
-            # next
-            p = p.next
-            pev = pev.next
-        return head
-
-    # produce two sub lists and concatnate them
-    def oddEvenListV2(self, head: ListNode) -> ListNode:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # produce two sub lists and concatnate them
         if not head:
             return None
         pod, pev, evhead = head, head.next, head.next
         while pod.next and pev.next:
             pod.next = pev.next
             pev.next = pod.next.next
-            pod = pod.next
-            pev = pev.next
+            pod, pev = pod.next, pev.next
         pod.next = evhead
         return head
 
 
 # TESTS
-tests = [
+for array, expected in [
     ([], []),
     ([1], [1]),
     ([1, 2], [1, 2]),
     ([1, 2, 3, 4, 5], [1, 3, 5, 2, 4]),
     ([1, 2, 3, 4, 5, 6], [1, 3, 5, 2, 4, 6]),
     ([2, 1, 3, 5, 6, 4, 7], [2, 3, 6, 7, 1, 5, 4]),
-]
-for t in tests:
+]:
     sol = Solution()
-    actual1 = ListNode.to_array(sol.oddEvenListV1(ListNode.from_array(t[0])))
-    actual2 = ListNode.to_array(sol.oddEvenListV2(ListNode.from_array(t[0])))
-    print("Odd even linked list of", t[0], "->", t[1])
-    assert actual1 == t[1] and actual2 == t[1]
+    actual = ListNode.to_array(sol.oddEvenList(ListNode.from_array(array)))
+    print("Odd even linked list of", array, "->", actual)
+    assert actual == expected

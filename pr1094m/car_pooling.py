247052e945e29daf61cca796d146ec3bba92ffec
l33tdaima <l33tdaima@github.com>
2022-01-05 21:25:33 -0500

    January 2022 Leetcoding Challenge: Revisited pr1094 Car Pooling in Python

--- pr1094m/car_pooling.py
+++ pr1094m/car_pooling.py
@@ -16,17 +16,15 @@ class Solution:
 
 
 # TESTS
-tests = [
+for trips, capacity, expected in [
     ([[2, 1, 5], [3, 3, 7]], 4, False),
     ([[2, 1, 5], [3, 3, 7]], 5, True),
     ([[2, 1, 5], [3, 5, 7]], 3, True),
     ([[3, 2, 7], [3, 7, 9], [8, 3, 9]], 11, True),
     ([[1, 8, 10], [3, 2, 7], [3, 6, 9], [8, 3, 9]], 14, True),
     ([[9, 3, 6], [8, 1, 7], [6, 6, 8], [8, 4, 9], [4, 2, 9]], 28, False),
-]
-for trips, capacity, expected in tests:
+]:
     sol = Solution()
     actual = sol.carPooling(trips, capacity)
     print("Trips", trips, ", capacity", capacity, "->", actual)
     assert actual == expected
-

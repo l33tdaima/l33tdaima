a5b80ea2296c386a7cf3c97a73f8b5cf1bd06ab4
l33tdaima <l33tdaima@github.com>
2022-01-13 23:02:28 -0500

    January 2022 Leetcoding Challenge: Revisited p008m String to Integer (atoi) in Python

--- p008m/atoi.py
+++ p008m/atoi.py
@@ -1,7 +1,7 @@
 class Solution:
-    def myAtoi(self, str: str) -> int:
+    def myAtoi(self, s: str) -> int:
         sign, result = 1, 0
-        for i, c in enumerate(str.lstrip()):
+        for i, c in enumerate(s.lstrip()):
             if i == 0 and c == "-":
                 sign = -1
                 continue
@@ -12,11 +12,11 @@ class Solution:
                 result = result * 10 + ord(c) - ord("0")
             else:
                 break
-        return max(-2 ** 31, min(sign * result, 2 ** 31 - 1))
+        return max(-(2 ** 31), min(sign * result, 2 ** 31 - 1))
 
 
 # TEST
-tests = [
+for s, expected in [
     ["", 0],
     [" -1", -1],
     ["+007", 7],
@@ -24,10 +24,8 @@ tests = [
     [" 4546 9090", 4546],
     ["4000000000", 2147483647],
     ["-3000000000", -2147483648],
-]
-for t in tests:
+]:
     sol = Solution()
-    actual = sol.myAtoi(t[0])
-    print("atoi(" + t[0] + ") ->", actual)
-    assert actual == t[1]
-
+    actual = sol.myAtoi(s)
+    print("atoi(" + s + ") ->", actual)
+    assert actual == expected

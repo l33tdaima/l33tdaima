1900e521a255892cd99209074da8c90d7fd434a3
l33tdaima <l33tdaima@github.com>
2022-01-16 23:54:30 -0500

    January 2022 Leetcoding Challenge: Revisited p290e Word Pattern in Python

--- p290e/README.md
+++ p290e/README.md
@@ -1,37 +1,45 @@
 # 290. Word Pattern (Easy)
 
-Given a pattern and a string str, find if str follows the same pattern.
+Given a `pattern` and a string `s`, find if `s` follows the same pattern.
 
-Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+Here follow means a full match, such that there is a bijection between a letter in `pattern` and a **non-empty** word in `s`.
 
 ### Example 1:
+
 ```
-Input: pattern = "abba", str = "dog cat cat dog"
+Input: pattern = "abba", s = "dog cat cat dog"
 Output: true
 ```
 
 ### Example 2:
+
 ```
-Input:pattern = "abba", str = "dog cat cat fish"
+Input:pattern = "abba", s = "dog cat cat fish"
 Output: false
 ```
 
 ### Example 3:
-```
-Input: pattern = "aaaa", str = "dog cat cat dog"
-Output: false
-```
 
-### Example 4:
 ```
-Input: pattern = "abba", str = "dog dog dog dog"
+Input: pattern = "aaaa", s = "dog cat cat dog"
 Output: false
 ```
 
 ### Notes:
+
 You may assume pattern contains only lowercase letters, and str contains lowercase letters separated by a single space.
 
+### Constraints:
+
+- `1 <= pattern.length <= 300`
+- `pattern` contains only lower-case English letters.
+- `1 <= s.length <= 3000`
+- `s` contains only lowercase English letters and spaces `' '`.
+- `s` _does not contain_ any leading or trailing spaces.
+- All the words in `s` are separated by a _single space_.
+
 ## Solution
+
 Both directions of mapping need to be stored.
 
 #UBER #DBX
--- p290e/word_pattern.py
+++ p290e/word_pattern.py
@@ -1,6 +1,9 @@
+from typing import Iterable
+
+
 class Solution:
-    def wordPatternV1(self, pattern: str, str: str) -> bool:
-        p2s, s2p, words = {}, {}, str.split()
+    def wordPatternV1(self, pattern: str, s: str) -> bool:
+        p2s, s2p, words = {}, {}, s.split()
         if len(pattern) != len(words):
             return False
         for p, s in zip(pattern, words):
@@ -14,23 +17,31 @@ class Solution:
                 s2p[s] = p
         return True
 
-    def wordPatternV2(self, pattern: str, str: str) -> bool:
+    def wordPatternV2(self, pattern: str, s: str) -> bool:
         # map the element of iterable xs to the index of first appearance
         f = lambda xs: map({}.setdefault, xs, range(len(xs)))
-        return list(f(pattern)) == list(f(str.split()))
+        return list(f(pattern)) == list(f(s.split()))
+
+    def wordPatternV2a(self, pattern: str, s: str) -> bool:
+        def f(xs: Iterable) -> list[int]:
+            d, ret = {}, []
+            for x, i in zip(xs, range(len(xs))):
+                ret.append(d.setdefault(x, i))
+            return ret
+
+        return f(pattern) == f(s.split())
 
 
 # TESTS
-tests = [
+for pattern, s, expected in [
     ("aaa", "aa aa aa aa", False),
     ("abba", "dog cat cat dog", True),
     ("abba", "dog cat cat fish", False),
     ("aaaa", "dog cat cat dog", False),
     ("abba", "dog dog dog dog", False),
-]
-for pattern, string, expected in tests:
+]:
     sol = Solution()
-    actual = sol.wordPatternV1(pattern, string)
-    print("String", string, "follows the same pattern", pattern, "->", actual)
+    actual = sol.wordPatternV2(pattern, s)
+    print("String", s, "follows the same pattern", pattern, "->", actual)
     assert actual == expected
-    assert expected == sol.wordPatternV2(pattern, string)
+    assert expected == sol.wordPatternV2a(pattern, s)

40316f2bd2a2ca86046a91c40b403f8c49073835
l33tdaima <l33tdaima@github.com>
2022-02-02 23:04:19 -0500

    Febuary 2021 Leetcoding Challenge: Revisited p454m 4Sum II in Python

--- p454m/four_sum_count.py
+++ p454m/four_sum_count.py
@@ -1,10 +1,9 @@
-from typing import List
 from collections import Counter
 
 
 class Solution:
     def fourSumCount(
-        self, A: List[int], B: List[int], C: List[int], D: List[int]
+        self, A: list[int], B: list[int], C: list[int], D: list[int]
     ) -> int:
         AB = Counter(a + b for a in A for b in B)
         return sum(AB[-c - d] for c in C for d in D)
@@ -13,6 +12,7 @@ class Solution:
 # TESTS
 for A, B, C, D, expected in [
     ([1, 2], [-2, -1], [-1, 2], [0, 2], 2),
+    ([0], [0], [0], [0], 1),
 ]:
     sol = Solution()
     actual = sol.fourSumCount(A, B, C, D)

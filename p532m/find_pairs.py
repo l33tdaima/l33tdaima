fb24accad9662d40ee01cff480a28d3ad8ccba92
l33tdaima <l33tdaima@github.com>
2022-02-08 22:47:23 -0500

    Febuary 2021 Leetcoding Challenge: Revisited p532m K-diff Pairs in an Array in Python

--- p532m/find_pairs.py
+++ p532m/find_pairs.py
@@ -1,11 +1,9 @@
-from typing import List
 from collections import Counter
 
 
 class Solution:
-    def findPairs(self, nums: List[int], k: int) -> int:
-        ans = 0
-        count = Counter(nums)
+    def findPairs(self, nums: list[int], k: int) -> int:
+        ans, count = 0, Counter(nums)
         for n in count:
             if k == 0 and count[n] > 1 or k > 0 and n + k in count:
                 ans += 1
@@ -13,14 +11,13 @@ class Solution:
 
 
 # TESTS
-tests = [
+for nums, k, expected in [
     ([3, 1, 4, 1, 5], 2, 2),
     ([1, 2, 3, 4, 5], 1, 4),
     ([1, 3, 1, 5, 4], 0, 1),
     ([1, 2, 4, 4, 3, 3, 0, 9, 2, 3], 3, 2),
     ([-1, -2, -3], 1, 2),
-]
-for nums, k, expected in tests:
+]:
     sol = Solution()
     actual = sol.findPairs(nums, k)
     print(f"The # of {k}-diff pairs in array", nums, "->", actual)

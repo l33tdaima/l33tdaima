7cbee4a38797e1427135c229bad6c50905f9529a
l33tdaima <l33tdaima@github.com>
2022-02-12 22:34:20 -0500

    Febuary 2021 Leetcoding Challenge: Revisited p078 Subsets in Python and JS

--- p078m/subsets.py
+++ p078m/subsets.py
@@ -1,8 +1,5 @@
-from typing import List
-
-
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsets(self, nums: list[int]) -> list[list[int]]:
         n, ans = len(nums), []
         for i in range(2 ** n):
             ans.append([nums[b] for b in range(n) if (i >> b) & 1])
@@ -10,7 +7,6 @@ class Solution:
 
 
 # TESTS
-tests = [[], [1], [1, 2], [1, 2, 3]]
-for t in tests:
+for t in [[], [1], [1, 2], [1, 2, 3]]:
     sol = Solution()
     print("Subsets of", t, "->\n", sol.subsets(t))

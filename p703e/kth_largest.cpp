186703dbe89d7b4bb3a9b49c1058452bee115310
l33tdaima <l33tdaima@github.com>
2022-04-07 23:43:31 -0400

    April 2022 Leetcoding Challenge: Revisited p703e Kth Largest Element in a Stream in Python

--- p703e/kth_largest.cpp
+++ p703e/kth_largest.cpp
@@ -1,25 +1,26 @@
 // g++ -std=c++11 *.cpp -o test && ./test && rm -f test
 
-#include <vector>
-#include <queue>
-#include <functional>
+#include <cassert>
 #include <iostream>
+#include <queue>
+#include <vector>
 
 using namespace std;
 
 class KthLargest {
-    priority_queue<int, vector<int>, greater<int> > d_minHeap;
+    priority_queue<int, vector<int>, greater<int>> d_minHeap;
     int d_cap;
+
 public:
-    KthLargest(int k, vector<int> nums)
-    : d_cap(k) {
-        for (int i = 0; i < nums.size(); ++i) {
-            if (i < k) d_minHeap.push(nums[i]);
-            else add(nums[i]);
-        }
+    KthLargest(int k, vector<int>& nums)
+        : d_cap(k)
+    {
+        for (int i = 0; i < nums.size(); ++i)
+            add(nums[i]);
     }
-    
-    int add(int val) {
+
+    int add(int val)
+    {
         if (d_minHeap.size() < d_cap) {
             d_minHeap.push(val);
         } else if (val > d_minHeap.top()) {
@@ -35,10 +36,10 @@ public:
  * KthLargest obj = new KthLargest(k, nums);
  * int param_1 = obj.add(val);
  */
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
     int k = 3;
-    vector<int> nums = {4, 5, 8, 2};
+    vector<int> nums = { 4, 5, 8, 2 };
     KthLargest kthLargest(k, nums);
     assert(kthLargest.add(3) == 4);
     assert(kthLargest.add(5) == 5);

e7db8156c53013fdb961730a76e648732f8c8827
l33tdaima <l33tdaima@github.com>
2022-10-18 22:48:05 -0400

    October 2022 Leetcoding Challenge: Revisited p692m Top K Frequent Words in C++

--- p692m/README.md
+++ p692m/README.md
@@ -1,32 +1,40 @@
 692. Top K Frequent Words (Medium)
 
-Given a non-empty list of words, return the k most frequent elements.
+Given an array of strings `words` and an integer `k`, return the `k` most frequent strings.
 
-Your answer should be sorted by frequency from highest to lowest. If two words have the same frequency, then the word with the lower alphabetical order comes first.
+Return the answer **sorted** by **the frequency** from highest to lowest. Sort the words with the same frequency by their **lexicographical order**.
 
 ### Example 1:
+
 ```
 Input: ["i", "love", "leetcode", "i", "love", "coding"], k = 2
 Output: ["i", "love"]
 Explanation: "i" and "love" are the two most frequent words.
-    Note that "i" comes before "love" due to a lower alphabetical order.
+Note that "i" comes before "love" due to a lower alphabetical order.
 ```
+
 ### Example 2:
+
 ```
 Input: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"], k = 4
 Output: ["the", "is", "sunny", "day"]
-Explanation: "the", "is", "sunny" and "day" are the four most frequent words,
-    with the number of occurrence being 4, 3, 2 and 1 respectively.
+Explanation: "the", "is", "sunny" and "day" are the four most frequent words, with the
+number of occurrence being 4, 3, 2 and 1 respectively.
 ```
-### Note:
-- You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
-- Input words contain only lowercase letters.
 
-### Follow up:
-Try to solve it in O(n log k) time and O(n) extra space.
+### Constraints:
+
+- `1 <= words.length <= 500`
+- `1 <= words[i].length <= 10`
+- `words[i]` consists of lowercase English letters.
+- `k` is in the range `[1, The number of unique words[i]]`
+
+### Follow-up: Could you solve it in O(n log(k)) time and O(n) extra space?
 
 ## Solution
+
 ### Heap Approach
+
 - Build a (word, count) hash table like the following for example 1, O(N) and O(N) extra space
   ```
   i 2
@@ -38,6 +46,7 @@ Try to solve it in O(n log k) time and O(n) extra space.
 - Pop the heap to output
 
 ### Trie Approach
+
 - Build a (word, count) hash table like the following for example 1, O(N) and O(N) extra space
 - Create a bucket of Tries, index is the frequency of words with the same freqency, size is O(N)
 - For each (word, count) in the hash table, add words into trie of the corresponding frequency(index)
--- p692m/top_k_frequent.cpp
+++ p692m/top_k_frequent.cpp
@@ -1,29 +1,29 @@
 // Build program by
-// g++ -std=c++11 *.cpp -o test && ./test && rm test
-#include <vector>
+// g++ -std=c++11 *.cpp -o test && ./test && rm -f test
+#include <iostream>
+#include <queue>
 #include <string>
 #include <unordered_map>
-#include <queue>
-#include <initializer_list>
-#include <iostream>
+#include <vector>
 
 using namespace std;
 class Solution {
     typedef pair<string, int> WordFreqType;
+
 public:
-    vector<string> topKFrequent(vector<string>& words, int k) {
+    vector<string> topKFrequent(vector<string>& words, int k)
+    {
         // Build a word freq map
         unordered_map<string, int> wordFreq;
-        for (const auto& w: words) {
-            wordFreq[w] ++;
-        }
+        for (const auto& w : words)
+            wordFreq[w]++;
         // Add them into a min heap
         auto comp = [](const WordFreqType& a, const WordFreqType& b) -> bool {
             return a.second > b.second || (a.second == b.second && a.first < b.first);
         };
-        typedef priority_queue< WordFreqType, vector<WordFreqType>, decltype(comp) > Heap;
+        typedef priority_queue<WordFreqType, vector<WordFreqType>, decltype(comp)> Heap;
         Heap minHeap(comp);
-        for (const auto& w: wordFreq) {
+        for (const auto& w : wordFreq) {
             minHeap.push(w);
             if (minHeap.size() > k) {
                 minHeap.pop();
@@ -40,25 +40,25 @@ public:
 };
 // TEST
 struct Test {
-    initializer_list<string> ilist;
+    vector<string> words;
     int k;
-    void run() const {
+    void run()
+    {
         Solution sol;
         cout << "Top " << k << " frequent words -> ";
-        vector<string> words(ilist);
         auto ans = sol.topKFrequent(words, k);
-        for (const auto& w: ans) {
+        for (const auto& w : ans)
             cout << w << " ";
-        }
         cout << endl;
     }
 };
-int main() {
-    initializer_list<Test> tests = {
-        {{"i", "love", "leetcode", "i", "love", "coding"}, 2},
-        {{"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"}, 4}
+int main()
+{
+    vector<Test> tests = {
+        { { "i", "love", "leetcode", "i", "love", "coding" }, 2 },
+        { { "the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is" }, 4 }
     };
-    for (const auto& t: tests) {
+    for (auto& t : tests) {
         t.run();
     }
     return 0;

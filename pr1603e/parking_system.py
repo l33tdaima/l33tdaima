5e410fc1dbcd2ecb46e53a7464a828ee9e8827c3
l33tdaima <l33tdaima@github.com>
2023-05-28 22:08:56 -0400

    May 2023 Leetcoding Challenge: Solved pr1603e Design Parking System in Python

---
+++ pr1603e/parking_system.py
@@ -0,0 +1,12 @@
+class ParkingSystem:
+    def __init__(self, big: int, medium: int, small: int):
+        self.spaces = [big, medium, small]
+
+    def addCar(self, carType: int) -> bool:
+        self.spaces[carType - 1] -= 1
+        return self.spaces[carType - 1] >= 0
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)

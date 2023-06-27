b26ce7ef83fed53a17d01bf03aa19f7bb527a46f
l33tdaima <l33tdaima@github.com>
2023-06-26 23:18:10 -0400

    June 2023 Leetcoding Challenge: Revisited p373m Find K Pairs with Smallest Sums in C++

--- p373m/README.md
+++ p373m/README.md
@@ -1,44 +1,56 @@
 # 373. Find K Pairs with Smallest Sums (Medium)
 
-You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+You are given two integer arrays `nums1` and `nums2` sorted in ascending order and an integer `k`.
 
-Define a pair (u,v) which consists of one element from the first array and one element from the second array.
+Define a pair `(u,v)` which consists of one element from the first array and one element from the second array.
 
-Find the k pairs (u1,v1),(u2,v2) ...(uk,vk) with the smallest sums.
+Find the `k` pairs `(u1,v1),(u2,v2) ...(uk,vk)` with the smallest sums.
 
 ### Example 1:
-```
-Given nums1 = [1,7,11], nums2 = [2,4,6],  k = 3
 
-Return: [1,2],[1,4],[1,6]
-
-The first 3 pairs are returned from the sequence:
-[1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
 ```
+Input: nums1 = [1,7,11], nums2 = [2,4,6],  k = 3
+
+Output: [1,2],[1,4],[1,6]
+
+Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+```
+
 ### Example 2:
-```
-Given nums1 = [1,1,2], nums2 = [1,2,3],  k = 2
 
-Return: [1,1],[1,1]
-
-The first 2 pairs are returned from the sequence:
-[1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
 ```
+Input: nums1 = [1,1,2], nums2 = [1,2,3],  k = 2
+
+Output: [1,1],[1,1]
+
+Explanation: The first 2 pairs are returned from the sequence: [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+```
+
 ### Example 3:
-```
-Given nums1 = [1,2], nums2 = [3],  k = 3 
 
-Return: [1,3],[2,3]
-
-All possible pairs are returned from the sequence:
-[1,3],[2,3]
 ```
+Input: nums1 = [1,2], nums2 = [3],  k = 3
+
+Output: [1,3],[2,3]
+
+Explanation: All possible pairs are returned from the sequence: [1,3],[2,3]
+```
+
+### Constraints:
+
+- `1 <= nums1.length, nums2.length <= 105`
+- `-109 <= nums1[i], nums2[i] <= 109`
+- `nums1` and `nums2` both are sorted in ascending order.
+- `1 <= k <= 10^4`
 
 ## Solution
+
 ### Intuitives Approach
+
 Let us say we have N1 numbers in nums1 and N2 numbers in nums2, we literally find the top k out of `N = N1 * N2` candidates. If using a heap of size K, we will do it in O(NlogK).
 
 ### Optimal Approach
+
 ```
       2   4   6
    +------------
@@ -46,7 +58,9 @@ Let us say we have N1 numbers in nums1 and N2 numbers in nums2, we literally fin
  7 |  9  11  13
 11 | 13  15  17
 ```
+
 Imagine we are looking at a matrix where each row and col are sorted, nums1 is row, and nums2 is col. The idea is to maintain a minheap of size N2,
+
 - Initialize by filling it by the first row
 - Iterate the following k times
   - Pop the min value from the heap
--- p373m/k_smallest_pairs.cpp
+++ p373m/k_smallest_pairs.cpp
@@ -1,39 +1,42 @@
 // Build program by
 // g++ -std=c++11 *.cpp -o test && ./test && rm test
 
-#include <vector>
-#include <utility>
-#include <queue>
 #include <iostream>
+#include <queue>
+#include <vector>
 
 using namespace std;
 
 class Solution {
     struct Tuple {
-        int u, v;
-        int i, j;
+        int u, v, i, j;
         bool operator<(Tuple rhs) const { return u + v > rhs.u + rhs.v; }
-        friend ostream& operator<<(ostream& os, const Tuple& t) {
+        friend ostream& operator<<(ostream& os, const Tuple& t)
+        {
             os << "(" << t.u << "," << t.v << "), [" << t.i << "," << t.j << "]";
             return os;
         }
     };
+
 public:
-    vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<pair<int, int>> ans;
-        if (nums1.size() == 0 || nums2.size() == 0) { return ans; }
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k)
+    {
+        vector<vector<int>> ans;
+        if (nums1.size() == 0 || nums2.size() == 0) {
+            return ans;
+        }
         priority_queue<Tuple> minHeap;
-        for (int j = 0; j < nums2.size(); ++j) {
-            Tuple tuple = {nums1[0], nums2[j], 0, j};
+        for (int j = 0; j < nums2.size() && j < k; ++j) {
+            Tuple tuple = { nums1[0], nums2[j], 0, j };
             minHeap.push(tuple);
         }
         while (!minHeap.empty() && k-- > 0) {
             int i = minHeap.top().i;
             int j = minHeap.top().j;
-            ans.push_back(make_pair(nums1[i], nums2[j]));
+            ans.push_back({ nums1[i], nums2[j] });
             minHeap.pop();
             if (i + 1 < nums1.size()) {
-                Tuple tuple = {nums1[i + 1], nums2[j], i + 1, j}; 
+                Tuple tuple = { nums1[i + 1], nums2[j], i + 1, j };
                 minHeap.push(tuple);
             }
         }
@@ -45,34 +48,36 @@ struct Test {
     vector<int> nums1;
     vector<int> nums2;
     int k;
-    void run() {
+    void run()
+    {
         cout << "The " << k << " smallest pairs -> ";
         Solution sol;
-        for (const auto& p: sol.kSmallestPairs(nums1, nums2, k)) {
-            cout << "[" << p.first << "," << p.second << "] ";
+        for (const auto& p : sol.kSmallestPairs(nums1, nums2, k)) {
+            cout << "[" << p[0] << "," << p[1] << "] ";
         }
         cout << endl;
     }
 };
-int main() {
+int main()
+{
     vector<Test> tests = {
         {
-            {1,7,11}, // nums1
-            {2,4,6},  // nums2
-            4         // k
+            { 1, 7, 11 }, // nums1
+            { 2, 4, 6 }, // nums2
+            4 // k
         },
         {
-            {1,1,2},  // nums1
-            {1,2,3},  // nums2
-            2         // k
+            { 1, 1, 2 }, // nums1
+            { 1, 2, 3 }, // nums2
+            2 // k
         },
         {
-            {1,2},    // nums1
-            {3},      // nums2
-            3         // k
+            { 1, 2 }, // nums1
+            { 3 }, // nums2
+            3 // k
         },
     };
-    for (Test& t: tests) {
+    for (Test& t : tests) {
         t.run();
     }
     return 0;
